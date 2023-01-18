e27038d31edebb7dc551a1088fbc8ee3e70cf042
Alberto Soutullo <alberto.soutullo639@gmail.com>
2023-01-18 16:35:52 +0100

    Modified build to get specific version of kurtosis

--- build.sh
+++ build.sh
@@ -2,6 +2,9 @@
 sudo apt-get update
 sudo apt-get install docker-ce docker-ce-cli containerd.io docker-compose-plugin
 
-# Install Kurtosis
-echo "deb [trusted=yes] https://apt.fury.io/kurtosis-tech/ /" | sudo tee /etc/apt/sources.list.d/kurtosis.list
-sudo apt install kurtosis-cli
+# Install Kurtosis, naively assuming amd64
+wget https://github.com/kurtosis-tech/kurtosis-cli-release-artifacts/releases/download/0.62.0/kurtosis-cli_0.62.0_linux_amd64.tar.gz
+tar -xf kurtosis-cli_0.62.0_linux_amd64.tar.gz
+mv kurtosis /usr/bin
+rm -r scripts
+rm kurtosis-cli_0.62.0_linux_amd64.tar.gz
