0aae5594cf54848e64d28d73b36976d8d1bfc4b4
Alberto Soutullo <alberto.soutullo639@gmail.com>
2023-01-18 15:06:28 +0100

    Changed run.sh to print only neccessary information, and also handle the gennet json to use it in Kurtosis

--- run.sh
+++ run.sh
@@ -1,40 +1,48 @@
 #!/bin/sh
 
+dir=$(pwd)
+
 # Parse arg if any
-ARGS1=${1:-"./config/config.json"}
-ARGS2=${2:-"gennet-module/config/gennet.yml"}
-echo $ARGS1
-echo $ARGS2
+ARGS1=${1:-"wakurtosis"}
+ARGS2=${2:-"./config/config.json"}
+ARGS3=${3:-"gennet-module/config/gennet.yml"}
 
 # Main .json configuration file
-kurtosis_config_file=$ARGS1
-gennet_config_file=$ARGS2
+enclave_name=$ARGS1
+kurtosis_config_file=$ARGS2
+gennet_config_file=$ARGS3
 
+echo "- Enclave name: " enclave_name
+echo "- Configuration file: " kurtosis_config_file
+echo "- Topology configuration: " gennet_config_file
 
 # Create and run gennet docker container
+echo -e "\nRunning topology generation with configuration:"
 cd gennet-module
-docker run --name gennet -v $PWD/gennet-module/:/gennet gennet
+docker run --name gennet-container -v ${dir}/gennet-module/config:/gennet/config gennet
 cd ..
 
 # Move output from gennet to config folder so kurtosis will use it
-mv gennet-module/topology/network_data.json config/network_topology/
-mv gennet-module/topology/*.toml config/waku_config_files
+echo -e "\nReplacing new topology data..."
+mv -f gennet-module/config/topology_generated/network_data.json config/network_topology/
+mv -f gennet-module/config/topology_generated/*.toml config/waku_config_files
 
-docker rm gennet
-docker volume rm $PWD/gennet-module/
+docker rm gennet-container > /dev/null 2>&1
 
-# Delete the enclave 
-kurtosis enclave rm -f $enclave_name
+# Delete the enclave just in case
+kurtosis enclave rm -f $enclave_name > /dev/null 2>&1
 
 # Create the new enclave and run the simulation
-kurtosis_cmd="kurtosis run --enclave-id ${enclave_name} . '{\"kurtosis_config_file\" : \"config/${kurtosis_config_file}\"}'"
+echo -e "\nInitiating enclave " $enclave_name
+kurtosis_cmd="kurtosis run --enclave-id ${enclave_name} . '{\"kurtosis_config_file\" : \"config/${kurtosis_config_file}\"}' > kurtosis_log.txt 2>&1"
 eval $kurtosis_cmd
+echo -e "Enclave " $enclave_name " is up and running"
 
 # Fetch the WSL service id and display the log of the simulation
-wsl_service_id=$(kurtosis enclave inspect wakurtosis | grep wsl- | awk '{print $1}')
-kurtosis service logs wakurtosis $wsl_service_id
-echo "--> To see simulation logs run: kurtosis service logs wakurtosis $wsl_service_id <--"
+wsl_service_id=$(kurtosis enclave inspect wakurtosis 2>/dev/null | grep wsl- | awk '{print $1}')
+# kurtosis service logs wakurtosis $wsl_service_id
+echo -e "\n--> To see simulation logs run: kurtosis service logs wakurtosis $wsl_service_id <--"
 
 # Fetch the Grafana address & port
-grafana_host=$(kurtosis enclave inspect wakurtosis | grep grafana- | awk '{print $6}')
-echo "--> Statistics in Grafana server at http://$grafana_host/ <--"
+grafana_host=$(kurtosis enclave inspect wakurtosis 2>/dev/null | grep grafana- | awk '{print $6}')
+echo -e "\n--> Statistics in Grafana server at http://$grafana_host/ <--"
