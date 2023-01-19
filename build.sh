1f714e96707a689adb6ec38aeb72061995b8a3a1
Alberto Soutullo <alberto.soutullo639@gmail.com>
2023-01-19 19:25:22 +0100

    Updated kurtosis version to 0.64.2

--- build.sh
+++ build.sh
@@ -3,8 +3,8 @@ sudo apt-get update
 sudo apt-get install docker-ce docker-ce-cli containerd.io docker-compose-plugin
 
 # Install Kurtosis, naively assuming amd64
-wget https://github.com/kurtosis-tech/kurtosis-cli-release-artifacts/releases/download/0.62.0/kurtosis-cli_0.62.0_linux_amd64.tar.gz
-tar -xf kurtosis-cli_0.62.0_linux_amd64.tar.gz
+wget https://github.com/kurtosis-tech/kurtosis-cli-release-artifacts/releases/download/0.64.2/kurtosis-cli_0.64.2_linux_amd64.tar.gz
+tar -xf kurtosis-cli_0.64.2_linux_amd64.tar.gz
 mv kurtosis /usr/bin
 rm -r scripts
-rm kurtosis-cli_0.62.0_linux_amd64.tar.gz
+rm kurtosis-cli_0.64.2_linux_amd64.tar.gz
