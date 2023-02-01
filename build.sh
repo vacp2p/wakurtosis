d7b3e35ab75594b3c36ddecae8ccfcb584336caa
Alberto Soutullo <alberto.soutullo639@gmail.com>
2023-02-01 16:30:13 +0100

    Changed kurtosis version to 0.65

--- build.sh
+++ build.sh
@@ -3,11 +3,11 @@ sudo apt-get update
 sudo apt-get install docker-ce docker-ce-cli containerd.io docker-compose-plugin
 
 # Install Kurtosis, naively assuming amd64
-wget https://github.com/kurtosis-tech/kurtosis-cli-release-artifacts/releases/download/0.64.2/kurtosis-cli_0.64.2_linux_amd64.tar.gz
-tar -xf kurtosis-cli_0.64.2_linux_amd64.tar.gz
+wget https://github.com/kurtosis-tech/kurtosis-cli-release-artifacts/releases/download/0.65.0/kurtosis-cli_0.65.0_linux_amd64.tar.gz --no-check-certificate
+tar -xf kurtosis-cli_0.65.0_linux_amd64.tar.gz
 mv kurtosis /usr/bin
 rm -r scripts
-rm kurtosis-cli_0.64.2_linux_amd64.tar.gz
+rm kurtosis-cli_0.65.0_linux_amd64.tar.gz
 
 # Build WSL and Gennet docker image
 cd wsl-module
