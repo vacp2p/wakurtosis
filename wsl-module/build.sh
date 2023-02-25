5ec4c78764e23d381c594b8493d1783fb4cbdc89
Gusto Bacvinka <augustinas.bacvinka@gmail.com>
2023-02-25 15:06:53 +0200

    Remove unused files

--- wsl-module/build.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-# pip freeze > requirements.txt
-image_id=$(docker images -q wsl:0.0.1)
-echo $image_id
-docker image rm -f $image_id
-docker image build --progress=plain  -t wsl:0.0.1 ./
