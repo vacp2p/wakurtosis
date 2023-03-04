557e149a2ed891e1386eb0fe328b18bfddedb5dc
Alberto Soutullo <alberto.soutullo639@gmail.com>
2023-03-04 20:58:53 +0100

    Added tests for Moved node builder star files to types folder.py

---
+++ wls-module/src/utils/tests/test_files.py
@@ -0,0 +1,33 @@
+import json
+import unittest
+import os
+
+from .. import files
+
+
+class TestFiles(unittest.TestCase):
+
+    def test_load_config_file(self):
+        config = files.load_config_file("test_files/test_config.json")
+        self.assertEqual(config["general"]["prng_seed"], 1234)
+        self.assertEqual(config["kurtosis"]["enclave_name"], "test")
+
+    def test_config_file_error(self):
+        with self.assertRaises(FileNotFoundError):
+            files.load_config_file("test_files/test_config_error.json")
+
+    def test_load_topology(self):
+        test_topology = files.load_topology("test_files/test_topology.json")
+        self.assertEqual(test_topology["containers"]["containers_0"][0], "node_0")
+        self.assertEqual(test_topology["nodes"]["node_0"]["image"], "nim-waku")
+
+    def test_load_topology_error(self):
+        with self.assertRaises(FileNotFoundError):
+            files.load_topology("test_files/test_topology_error.json")
+
+    def test_save_messages_to_json(self):
+        msgs_dict = {"test": "test"}
+        files.save_messages_to_json(msgs_dict)
+        with open("messages.json", "r") as f:
+            self.assertEqual(json.load(f), msgs_dict)
+        os.remove("messages.json")
