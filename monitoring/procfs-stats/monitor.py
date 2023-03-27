feea5695ba67f038c2f4546f0f35927d4663708d
0xFugue <119708655+0xFugue@users.noreply.github.com>
2023-03-27 23:52:38 +0530

    renaming,bug fix,  signal handling

--- monitoring/procfs-stats/monitor.py
+++ monitoring/procfs-stats/monitor.py
@@ -15,32 +15,32 @@ OEXT="out"
 # TODO: return only relevant bits for CPU
 def get_cpu_metrics(f):
     f.seek(0)
-    return f.read()
+    return f"CPU {f.read()}"
 
 
 def get_mem_metrics(f, lst):
     f.seek(0)
     rbuff = f.readlines()
-    res = ''.join([rbuff[i].replace("\t", " ").replace("\n", ", ").replace(":", " ") for i in lst])
-    return res
+    res = ''.join([rbuff[i].replace("\t", " ").replace("\n", " ").replace(":", " ") for i in lst])
+    return f"MEM {res}"
 
 
-def get_net_metrics(f, lst):
+def get_net_metrics(f, lst=[]): #lst might be needed in the future
     f.seek(0)
     rbuff = f.readlines()
     res =  ''.join([line.replace("\t", " ").replace("\n", " ") for line in rbuff if "eth" in line])
-    return res
+    return f"NET {res}"
 
 
 def get_blk_metrics(f, lst):
     f.seek(0)
     rbuff = f.readlines()
-    res = ''.join([rbuff[i].replace("\n", ", ") for i in lst])
+    #sysfs is dodgy at times
+    if rbuff == ['Total 0\n']:
+        res = "BLK Read 0  Write 0 "
+    else:
+        res = ''.join([rbuff[i].replace("\n", " ") for i in lst])
     return res
-
-
-def signal_handler(sig, frame):
-    sys.exit(0)
 
 
 class MetricsCollector:
@@ -91,17 +91,14 @@ class MetricsCollector:
         return self.docker_name2id[self.docker_pid2name[pid]]
 
     def populate_file_handles_1(self):
+        assert self.docker_name2id != {} and self.docker_pid2name != {}
         # pid to file handles
         self.pid2procfds[0]["cpu"] =  open(f"/proc/stat")
         for pid in self.docker_pids:
             self.pid2procfds[pid]["cpu"] =  open(f"/proc/{pid}/stat")
             self.pid2procfds[pid]["mem"] =  open(f"/proc/{pid}/status") #grep ^VmPeak VmRSS /proc/*/status 
             self.pid2procfds[pid]["net"] =  open(f"/proc/{pid}/net/dev")
-            self.pid2procfds[pid]["blk"] =  open((f"/sys/fs/cgroup/blkio/docker/"
-                                                  f"{self.pid2docker(pid)}/"
-                                                  f"blkio.throttle.io_service_bytes"
-                                                ))
-            #self.pid2procfds[pid]["blk"] =  open(f"/proc/{pid}/io") # per-procses, but needs SUDO
+            self.pid2procfds[pid]["blk"] =  open(f"/sys/fs/cgroup/blkio/docker/{self.pid2docker(pid)}/blkio.throttle.io_service_bytes")
         self.procfs_fd = open(f"{OPREFIX}-{self.procfs_fname}.{OEXT}", "w")
             #pid2procfds[pid]["net"] =  open(f"/proc/{pid}/")
 
@@ -157,14 +154,17 @@ class MetricsCollector:
         stat = " ".join(get_cpu_metrics(self.pid2procfds[0]["cpu"]).splitlines())
         for pid in self.docker_pids:
             docker_id = self.pid2docker(pid)
-            cpu = get_cpu_metrics(self.pid2procfds[pid]["cpu"]).strip() + " , "
-            mem = get_mem_metrics(self.pid2procfds[pid]["mem"], [16, 17, 21]) #VmPeak, VmSize, VmRSS
-            net = get_net_metrics(self.pid2procfds[pid]["net"], [0, 1]) # Read, Write
-            blk = get_blk_metrics(self.pid2procfds[pid]["blk"], [0, 1]) # Read, Write
-            #blk = get_blk_metrics(self.pid2procfds[pid]["blk"], [4, 5]) # Read, Write
+            try:
+                cpu = get_cpu_metrics(self.pid2procfds[pid]["cpu"]).strip()
+                mem = get_mem_metrics(self.pid2procfds[pid]["mem"], [16, 17, 21]) #VmPeak, VmSize, VmRSS
+                net = get_net_metrics(self.pid2procfds[pid]["net"]) # Whole row
+                blk = get_blk_metrics(self.pid2procfds[pid]["blk"], [0, 1]) # Read, Write
+            except:
+                continue
+            #blk = get_blk_metrics(self.pid2procfds[pid]["blk"], [4, 5]) # Read, Write, SUDO
             out = ( f"SAMPLE_{self.procfs_sample_cnt} "
-                    f"{docker_id} {time.time()} {mem} "
-                    f"{net} {blk} {cpu} {''.join(stat)}\n"
+                    f"{docker_id} {time.time()} "
+                    f"{mem} {net} {blk} {cpu}\n"
                   )
             #out = f"SAMPLE_{cnt} {time.time()} {mem}# {net}# {blk} {''.join(stat)}\n"
             #print(str(out))
@@ -182,12 +182,11 @@ class MetricsCollector:
         self.procfs_scheduler.run()
 
 
-    def build_the_basics(self, dps_fname, dinspect_fname, cpuinfo_fname, meminfo_fname):
+    def record_the_basics(self, dps_fname, dinspect_fname, cpuinfo_fname, meminfo_fname):
         self.build_and_exec(self.docker_ps, dps_fname)
         self.build_and_exec(self.docker_inspect, dinspect_fname)
         self.build_and_exec("cat /proc/cpuinfo", cpuinfo_fname)
         self.build_and_exec("cat /proc/meminfo", meminfo_fname)
-
         self.docker_ps_fname, self.docker_inspect_fname = f"{OPREFIX}-{dps_fname}.{OEXT}", f"{OPREFIX}-{dinspect_fname}.{OEXT}"
 
     def build_docker_pids_1(self):
@@ -214,14 +213,20 @@ class MetricsCollector:
         log.info(f"docker: pids : {str(self.pids)}")
 
     def build_metadata(self, dps_fname, dinspect_fname, cpuinfo_fname, meminfo_fname, csize=1):
-        self.build_the_basics(dps_fname, dinspect_fname, cpuinfo_fname, meminfo_fname)
+        self.record_the_basics(dps_fname, dinspect_fname, cpuinfo_fname, meminfo_fname)
         if self.csize == 1:
             self.build_docker_pids_1()
         else:
             self.build_docker_pids_n()
 
+    def register_signals(self):
+        signal.signal(signal.SIGINT, self.signal_handler)
+        signal.signal(signal.SIGTERM, self.signal_handler)
+        signal.signal(signal.SIGQUIT, self.signal_handler)
+
     def spin_up(self, dstats_fname, procfs_fname):
         log.info("Starting the docker monitor...")
+        self.register_signals()
         self.docker_thread = threading.Thread(
                 target=self.launch_docker_monitor, args=(dstats_fname,), daemon=True)
         self.docker_thread.start()
@@ -235,11 +240,14 @@ class MetricsCollector:
         os.kill(self.docker_stats_pid, signal.SIGTERM)
         log.info(f"docker monitor stopped : {self.docker_stats_pid}, {self.docker_stats_fname}")
 
+    def signal_handler(self, sig, frame):
+        self.clean_up()
+        sys.exit(0)
+
 def main(ctx: typer.Context):
     format = "%(asctime)s: %(message)s"
     log.basicConfig(format=format, level=log.INFO,
                         datefmt="%H:%M:%S")
-    signal.signal(signal.SIGINT, signal_handler)
 
     log.info("Metrics : starting")
     metrics = MetricsCollector(csize=1, sampling_interval=1)
