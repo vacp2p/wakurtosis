fd8de0488d2a883c31bfc500bf44e9d1bd5715a9
0xFugue <119708655+0xFugue@users.noreply.github.com>
2023-03-29 02:09:59 +0530

    comments

--- monitoring/procfs-stats/monitor.py
+++ monitoring/procfs-stats/monitor.py
@@ -9,16 +9,20 @@ from collections import defaultdict
 import sched, time
 import typer
 
+# output file loc and name formats
 ODIR="stats"
 OPREFIX=f'{ODIR}/docker'
 OEXT="out"
 
-# TODO: return only relevant bits to compute the percentages for CPU
+# max number of wakunodes per container
+MAX_CSIZE=10
+
+# TODO: return only relevant fields to compute the percentages for CPU
 def get_cpu_metrics(f):
     f.seek(0)
     return f'CPU {f.read()}'
 
-
+# pulls VmPeak, VmSize, VmRSS stats per wakunode
 def get_mem_metrics(f):
     f.seek(0)
     rbuff = f.readlines()
@@ -27,22 +31,19 @@ def get_mem_metrics(f):
     return f'MEM {res}'
 
 
-# TODO: return only Send/Recv
+# TODO: return only Send/Recv fields
 def get_net_metrics(f):
     f.seek(0)
     rbuff = f.readlines()
     res =  ''.join([line.replace("\t", " ").replace("\n", " ") for line in rbuff if "eth" in line])
     return f'NET {res}'
 
-
-def get_blk_metrics(f, csize):
+# pulls the read/write bytes per wakunodes
+def get_blk_metrics(f):
     f.seek(0)
     rbuff = f.readlines()
-    lst = [0, 1] if csize == 1 else [4, 5]
-    try: #sysfs is dodgy at times
-        res = ''.join([rbuff[i].replace("\n", " ").replace("259:0 ", "") for i in lst])
-    except:
-        res = "Read 0  Write 0 "
+    lst = [4, 5]        #lst = [0, 1] if csize == 1 else [4, 5]
+    res = ''.join([rbuff[i].replace("\n", " ").replace("259:0 ", "") for i in lst])
     return f'BLK {res}'
 
 
@@ -81,17 +82,12 @@ class MetricsCollector:
         # self.mem_usage_abs = -1 self.mem_usage_perc = -1 self.net_sends = -1
         # self.net_recvs = -1 self.block_reads = -1 self.block_writes = -1
 
+    # build, exec, and collect output
     def build_and_exec(self, cmd, fname):
         cmdline = f'exec {cmd} > {OPREFIX}-{fname}.{OEXT}'
         subprocess.run(cmdline, shell=True)
 
-    def launch_docker_monitor(self, dstats_fname):
-        cmd = f'exec {self.docker_stats} > {OPREFIX}-{dstats_fname}.{OEXT}'
-        log.debug("docker monitor cmd : " +  cmd)
-        process = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE, preexec_fn=os.setsid)
-        self.docker_stats_pid, self.docker_stats_fname = process.pid, dstats_fname
-        log.info(f'docker monitor started : {self.docker_stats_pid}, {self.docker_stats_fname}')
-
+    # open the /proc files ahead to save up parsing, lookup, fd alloc etc.
     def populate_file_handles(self):
         assert self.docker_pid2name != {} and self.docker_name2id != {} and self.docker_pid2id != {}
         self.pid2procfds[0]["cpu"] =  open(f'/proc/stat')
@@ -99,17 +95,14 @@ class MetricsCollector:
             self.pid2procfds[pid]["cpu"] =  open(f'/proc/{pid}/stat')
             self.pid2procfds[pid]["mem"] =  open(f'/proc/{pid}/status')
             self.pid2procfds[pid]["net"] =  open(f'/proc/{pid}/net/dev')
-            blk = ((f'/sys/fs/cgroup/blkio/docker'
-                    f'/{self.docker_pid2id[pid]}/'
-                    f'blkio.throttle.io_service_bytes'
-                 )) if self.csize == 1 else f'/proc/{pid}/io'
-            try:
-                self.pid2procfds[pid]["blk"] =  open(blk) # require SUDO for csize > 1
-            except:
-                log.error(f"ERROR: You need root permissions to read {blk}")
-                os._exit(-1)
+            #blk = ((f'/sys/fs/cgroup/blkio/docker'
+            #        f'/{self.docker_pid2id[pid]}/'
+            #        f'blkio.throttle.io_service_bytes'
+            #     )) if self.csize == 1 else f'/proc/{pid}/io'
+            self.pid2procfds[pid]["blk"] =  open(f'/proc/{pid}/io') # require SUDO
         self.procfs_fd = open(f'{OPREFIX}-{self.procfs_fname}.{OEXT}', "w")
 
+    # close all the opened file handles
     def teardown_file_handles(self):
         self.pid2procfds[0]["cpu"].close()
         for pid in self.docker_pids:
@@ -119,13 +112,22 @@ class MetricsCollector:
             #self.pid2procfds[pid]["blk"].close()
         self.procfs_fd.close()
 
+    # launches the docker monitor and saves the pid for later use
+    def launch_docker_monitor(self, dstats_fname):
+        cmd = f'exec {self.docker_stats} > {OPREFIX}-{dstats_fname}.{OEXT}'
+        log.debug("docker monitor cmd : " +  cmd)
+        process = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE, preexec_fn=os.setsid)
+        self.docker_stats_pid, self.docker_stats_fname = process.pid, dstats_fname
+        log.info(f'docker monitor started : {self.docker_stats_pid}, {self.docker_stats_fname}')
+
+    # the /proc reader : this is is in fast path
     def procfs_reader(self):
         cpu_stat = " ".join(get_cpu_metrics(self.pid2procfds[0]["cpu"]).splitlines())
         for pid in self.docker_pids:
             cpu = get_cpu_metrics(self.pid2procfds[pid]["cpu"]).strip() # all cpu stats
             mem = get_mem_metrics(self.pid2procfds[pid]["mem"])
             net = get_net_metrics(self.pid2procfds[pid]["net"]) # Whole eth* row
-            blk = get_blk_metrics(self.pid2procfds[pid]["blk"], self.csize) # Read, Write
+            blk = get_blk_metrics(self.pid2procfds[pid]["blk"]) # Read, Write
             out = ( f'SAMPLE_{self.procfs_sample_cnt} '
                     f'{pid} {time.time()} '         # file has pid to docker_id map
                     f'{mem} {net} {blk} {cpu} {"".join(cpu_stat)}\n'
@@ -136,6 +138,7 @@ class MetricsCollector:
         self.procfs_sample_cnt += 1
         self.procfs_scheduler.enter(self.procfs_sampling_interval, 1, self.procfs_reader, ())
 
+    # add headers and schedule /proc reader's first read
     def launch_procfs_monitor(self, procfs_fname):
         self.procfs_fname = procfs_fname
         self.populate_file_handles()    # including procfs_fd
@@ -148,6 +151,7 @@ class MetricsCollector:
                      self.procfs_reader, ())
         self.procfs_scheduler.run()
 
+    # collect and record the basic info about the system and running dockers
     def record_the_basics(self, dps_fname, dinspect_fname, cpuinfo_fname, meminfo_fname):
         self.build_and_exec(self.docker_ps, dps_fname)
         self.build_and_exec(self.docker_inspect, dinspect_fname)
@@ -160,10 +164,12 @@ class MetricsCollector:
                 self.docker_name2id[l[1]] = l[0]
                 self.docker_ids.append(l[0])
 
+    # build the process pid to docker id map
     def build_pid2id(self):
         self.docker_pid2id = {pid :
                 self.docker_name2id[self.docker_pid2name[pid]] for pid in self.docker_pids}
 
+    # build the process pid to docker name map for one wakunode per container
     def build_pid2name_1(self):
         with open(self.docker_inspect_fname, "r") as f:
             for line in f:
@@ -173,6 +179,7 @@ class MetricsCollector:
         log.debug(f'docker: ids, pids : {str(self.docker_ids)}, {str(self.docker_pids)}')
         log.debug(f'docker: maps : {self.docker_pid2name}, {self.docker_name2id}')
 
+    # build the process pid to docker name map : will include non-docker wakunodes
     def build_pid2name_n(self):
         log.debug(self.ps_docker)
         self.build_and_exec(self.ps_docker, self.ps_pids_fname)
@@ -208,6 +215,7 @@ class MetricsCollector:
                     self.docker_pid2name[pid] = docker_name
                     os.remove(f'{OPREFIX}-name-{pid}.{OEXT}')
 
+    # build metadata for the runs: about docker, build name2id, pid2name and pid2id maps
     def build_metadata(self, dps_fname, dinspect_fname, cpuinfo_fname, meminfo_fname):
         self.record_the_basics(dps_fname, dinspect_fname, cpuinfo_fname, meminfo_fname)
         if self.csize == 1:
@@ -217,11 +225,7 @@ class MetricsCollector:
         log.info(f'docker: waku pids : {str(self.docker_pids)}')
         self.build_pid2id()
 
-    def register_signals(self):
-        signal.signal(signal.SIGINT, self.signal_handler)
-        signal.signal(signal.SIGTERM, self.signal_handler)
-        signal.signal(signal.SIGQUIT, self.signal_handler)
-
+    # after metadata is collected, create the threads and launch data collection
     def spin_up(self, dstats_fname, procfs_fname):
         log.info("Starting the docker monitor...")
         self.register_signals()
@@ -233,26 +237,35 @@ class MetricsCollector:
                 target=self.launch_procfs_monitor, args=(procfs_fname,), daemon=True)
         self.procfs_thread.start()
 
+    # kill docker stats : always kill, never TERM/QUIT/INT
     def terminate_docker_stats(self):
         log.info(f'stopping docker monitor : {self.docker_stats_pid}, {self.docker_stats_fname}')
-        os.kill(self.docker_stats_pid, signal.SIGKILL) # cannot use TERM/QUIT/INT
+        os.kill(self.docker_stats_pid, signal.SIGKILL) # do not use TERM/QUIT/INT
 
+    # the cleanup: close the file handles and kill the docker
     def clean_up(self):
         self.teardown_file_handles()
         self.terminate_docker_stats()
 
+    # register interruptible signals' handlers
+    def register_signals(self):
+        signal.signal(signal.SIGINT, self.signal_handler)
+        signal.signal(signal.SIGTERM, self.signal_handler)
+        signal.signal(signal.SIGQUIT, self.signal_handler)
+
+    # the signal handler
     def signal_handler(self, sig, frame):
         self.clean_up()
         sys.exit(0)
 
-
-MAX_CSIZE=10
+# ensure 0 < container size <= MAX_SIZE
 def _csize_callback(ctx: typer, Context, csize: int):
     if csize <= 0 or csize > MAX_CSIZE:
         raise ValueError(f"container_size must be an int in (0, {MAX_CSIZE}]")
     return csize
 
 
+# ensure 0 < sampling_interval
 def _sinterval_callback(ctx: typer, Context, sinterval: int):
     if sinterval <= 0 :
         raise ValueError(f"sampling_interval must be > 0")
@@ -268,15 +281,15 @@ def main(ctx: typer.Context,
     format = "%(asctime)s: %(message)s"
     log.basicConfig(format=format, level=log.INFO,
                         datefmt="%H:%M:%S")
-
     pathlib.Path(ODIR).mkdir(parents=True, exist_ok=True)
+
     log.info("Metrics : starting")
     metrics = MetricsCollector(csize=container_size, sampling_interval=sampling_interval)
 
-    log.info("Collecting system and container metadata...")
+    log.info("Metrics: Collecting system and container metadata...")
     metrics.build_metadata("ps", "inspect", "cpuinfo", "meminfo")
 
-    log.info("Starting the Measurement Threads")
+    log.info("Metrics: Starting the data collection threads")
     metrics.spin_up("stats", "procfs")
 
     # get sim time info from config.json? or  ioctl/select from WLS? or docker wait?
@@ -284,7 +297,9 @@ def main(ctx: typer.Context,
 
     # x.join()
 
+    log.info("Metrics : clean up")
     metrics.clean_up()
+
     log.info("Metrics : all done")
 
 
