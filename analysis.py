5e31b4a18c794e8d242b7e7f8d43065a41b8ad59
Alberto Soutullo <alberto.soutullo639@gmail.com>
2023-03-28 18:43:00 +0200

    Changed code to work with multinode and starting to refactor

--- analysis.py
+++ analysis.py
@@ -179,9 +179,69 @@ def fetch_cadvisor_stats_from_container(container_id, start_ts, end_ts):
     return {'cpu_usage' : cpu_usage, 'memory_usage' : memory_usage}
 
 
-def main():
+def _load_topics(node_info, nodes, node):
+    topics = None
+    with open("config/topology_generated/" + node_info["node_config"], mode='rb') as read_file:
+        toml_config = tomllib.load(read_file)
+        if node_info["image"] == "nim-waku":
+            topics = list(toml_config["topics"].split(" "))
+        elif node_info["image"] == "go-waku":
+            topics = toml_config["topics"]
+        else:
+            raise ValueError("Unknown image type")
+    # Load topics into topology for easier access
+    nodes[node]["topics"] = topics
+
+
+def load_topics_into_topology(topology):
+    """ Load Topics """
+    nodes = topology["nodes"]
+    for node, node_info in nodes.items():
+        try:
+            _load_topics(node_info, nodes, node)
+        except ValueError as e:
+            G_LOGGER.error('%s: %s' % (e.__doc__, e))
+            sys.exit()
+
+    G_LOGGER.info('Loaded nodes topics from toml files')
+
+
+def load_topology(topology_file):
+    """ Load topology """
+    with open(topology_file, 'r') as read_file:
+        topology = json.load(read_file)
+
+    G_LOGGER.debug(topology)
+    G_LOGGER.info('Topology loaded')
+
+    return topology
+
+
+def load_messages(simulation_path):
+    try:
+        with open(f'{simulation_path}/messages.json', 'r') as f:
+            injected_msgs_dict = json.load(f)
+    except OSError as e:
+        G_LOGGER.error('%s: %s' % (e.__doc__, e))
+        sys.exit()
+
+    G_LOGGER.info(f'Loaded {len(injected_msgs_dict)} messages.')
+
+    return injected_msgs_dict
+
+
+def compare_tss(tss, min_tss, max_tss):
+    if tss < min_tss:
+        min_tss = tss
+    elif tss > max_tss:
+        max_tss = tss
+
+    return min_tss, max_tss
+
+
+def _innit_logger():
     global G_LOGGER
-    
+
     """ Init Logging """
     G_LOGGER = logging.getLogger(G_APP_NAME)
     handler = logging.StreamHandler(sys.stdout)
@@ -193,116 +253,108 @@ def main():
     # Set loglevel from config
     G_LOGGER.setLevel(G_LOG_LEVEL)
     handler.setLevel(G_LOG_LEVEL)
-    
+
     G_LOGGER.info('Started')
 
+
+def _parse_args():
     """ Parse command line args """
     parser = argparse.ArgumentParser()
-    parser.add_argument("-sp", "--simulation_path", help="Simulation results path", default=G_DEFAULT_SIMULATION_PATH)
+    parser.add_argument("-sp", "--simulation_path", help="Simulation results path",
+                        default=G_DEFAULT_SIMULATION_PATH)
     args = parser.parse_args()
 
     simulation_path = args.simulation_path
+
+    return simulation_path
+
+
+def get_relay_line_info(log_line):
+    msg_topics = re.search(r'topics="([^"]+)"', log_line).group(1)
+    msg_topic = re.search(r'pubsubTopic=([^ ]+)', log_line).group(1)
+    msg_hash = re.search(r'hash=([^ ]+)', log_line).group(1)
+    msg_peer_id = re.search(r'peerId=([^ ]+)', log_line).group(1)
+
+    return msg_topics, msg_topic, msg_hash, msg_peer_id
+
+
+def main():
+    simulation_path = _parse_args()
         
     """ Load Topics Structure """
-    topics = set()
-    nodes_topics = [] 
-    try:
-        tomls = glob.glob('%s/*.toml' %G_DEFAULT_TOPOLOGY_PATH)
-        # Index is the node id
-        tomls.sort()
-        for toml_file in tomls:
-            
-            with open(toml_file, mode='rb') as read_file:
-                toml_config = tomllib.load(read_file)
-                node_topics_str = toml_config['topics']
-                topics_list = list(node_topics_str.split(' '))
-                nodes_topics.append(topics_list)
-                topics.update(topics_list)
-    except Exception as e:
-        G_LOGGER.error('%s: %s' % (e.__doc__, e))
-        sys.exit()
-
-    G_LOGGER.info('Loaded topic structure with %d topic(s) and %d node(s).' %(len(topics), len(nodes_topics)))
-    # G_LOGGER.debug(topics)
-    # G_LOGGER.debug(nodes_topics)
+    topology = load_topology(simulation_path + "/network_data.json")
+    load_topics_into_topology(topology)
 
     """ Load Simulation Messages """
-    injected_msgs_dict = {}
-    try:
-        with open('%s/messages.json' %simulation_path, 'r') as f:
-            injected_msgs_dict = json.load(f)
-    except Exception as e:
-        G_LOGGER.error('%s: %s' % (e.__doc__, e))
-        sys.exit()
-
-    G_LOGGER.info('Loaded %d messages.' %len(injected_msgs_dict))
-    # G_LOGGER.debug(injected_msgs_dict)
+    injected_msgs_dict = load_messages(simulation_path)
 
     node_logs = {}
     msgs_dict = {}
-    
-    # Helper list with all the timestamps
-    tss = []
-    try:
-        services_log_paths = glob.glob('%s/*--user-service--*' %simulation_path)
-    
-        pbar = tqdm(services_log_paths)
-        for log_path in pbar:
-            with open('%s/spec.json' %log_path, mode='r') as f:
-                spec_json = json.load(f)
-                if spec_json['Path'] == '/usr/bin/wakunode':
-                    node_id = spec_json['Config']['Labels']['com.kurtosistech.id']
-                    
-                    # container_id = spec_json['Name'][1:]
-                    container_id = spec_json['Id']
-                    node_logs[node_id] = {'published' : [], 'received' : [], 'container_id' : container_id}
-                    
-                    pbar.set_description("Parsing log of node %s" %node_id)
 
-                    with open('%s/output.log' %log_path, mode='r') as f:
-                        
-                        # Process log line by line as a text string
-                        for log_line in f:
-                            
-                            # At this stage we only care about Waku Relay protocol
-                            if 'waku.relay' in log_line:
-                                
-                                msg_topics = re.search(r'topics="([^"]+)"', log_line).group(1)
-                                msg_topic = re.search(r'pubsubTopic=([^ ]+)', log_line).group(1)
-                                msg_hash = re.search(r'hash=([^ ]+)', log_line).group(1)
+    max_tss = -sys.maxsize - 1
+    min_tss = sys.maxsize
 
-                                if 'published' in log_line:
-                                    msg_publishTime = int(re.search(r'publishTime=([\d]+)', log_line).group(1))
-                                    tss.append(msg_publishTime)
+    for container_name, nodes_in_container in topology["containers"].items():
+        node_pbar = tqdm(nodes_in_container)
 
-                                    node_logs[node_id]['published'].append([msg_publishTime, msg_topics, msg_topic, msg_hash])
-                                    
-                                    if msg_hash not in msgs_dict:
-                                        msgs_dict[msg_hash] = {'published' : [{'ts' : msg_publishTime, 'node_id' : node_id}], 'received' : []}
-                                    else:
-                                        msgs_dict[msg_hash]['published'].append({'ts' : msg_publishTime, 'node_id' : node_id})
-                                    
-                                elif 'received' in log_line: 
-                                    msg_receivedTime = int(re.search(r'receivedTime=([\d]+)', log_line).group(1))
-                                    tss.append(msg_receivedTime)
-                                    
-                                    node_logs[node_id]['received'].append([msg_receivedTime, msg_topics, msg_topic, msg_hash])
-                                    
-                                    if msg_hash not in msgs_dict:
-                                        msgs_dict[msg_hash] = {'published' : [], 'received' : [{'ts' : msg_receivedTime, 'node_id' : node_id}]}
-                                    else:
-                                        msgs_dict[msg_hash]['received'].append({'ts' : msg_receivedTime, 'node_id' : node_id})
-                                                                    
-                    G_LOGGER.debug('Parsed node \"%s\" log in %s/output.log' %(node_id, log_path))
-    except Exception as e:
-        G_LOGGER.error('%s: %s' % (e.__doc__, e))
-        sys.exit()
+        node_pbar.set_description(f"Parsing log of container {container_name}")
+
+        for node in node_pbar:
+            node_info = topology["nodes"][node]
+            peer_id = node_info["peer_id"][:3] + "*" + node_info["peer_id"][-6:]
+            node_logs[peer_id] = {'published': [], 'received': [],
+                               'container_name': container_name, 'peer_id': node}
+
+        folder = glob.glob(f'{simulation_path}/{container_name}*')
+        if len(folder) > 1:
+            raise RuntimeError(f"Error: Multiple containers with same name: {folder}")
+
+        try:
+            file = open(f'{folder[0]}/output.log', mode='r')
+        except OSError as e:
+            G_LOGGER.error(f'{e.__doc__}: {e}')
+            sys.exit()
+
+        for log_line in file:
+            if 'waku.relay' in log_line:
+                msg_topics, msg_topic, msg_hash, msg_peer_id = get_relay_line_info(log_line)
+
+                if 'published' in log_line:
+                    msg_publishTime = int(re.search(r'publishTime=([\d]+)', log_line).group(1))
+                    min_tss, max_tss = compare_tss(msg_publishTime, min_tss, max_tss)
+
+                    node_logs[msg_peer_id]['published'].append(
+                        [msg_publishTime, msg_topics, msg_topic, msg_hash])
+
+                    if msg_hash not in msgs_dict:
+                        msgs_dict[msg_hash] = {
+                            'published': [{'ts': msg_publishTime, 'peer_id': msg_peer_id}],
+                            'received': []}
+                    else:
+                        msgs_dict[msg_hash]['published'].append(
+                            {'ts': msg_publishTime, 'peer_id': msg_peer_id})
+
+                elif 'received' in log_line:
+                    msg_receivedTime = int(re.search(r'receivedTime=([\d]+)',
+                                                     log_line).group(1))
+                    min_tss, max_tss = compare_tss(msg_receivedTime, min_tss, max_tss)
+
+                    node_logs[msg_peer_id]['received'].append(
+                        [msg_receivedTime, msg_topics, msg_topic, msg_hash])
+
+                    if msg_hash not in msgs_dict:
+                        msgs_dict[msg_hash] = {'published': [], 'received': [
+                            {'ts': msg_receivedTime, 'peer_id': msg_peer_id}]}
+                    else:
+                        msgs_dict[msg_hash]['received'].append(
+                            {'ts': msg_receivedTime, 'peer_id': msg_peer_id})
+
+        file.close()
 
     # Compute simulation time window
-    simulation_start_ts = min(tss)
-    simulation_end_ts = max(tss)
-    simulation_time_ms = round((simulation_end_ts - simulation_start_ts) / 1000000)
-    G_LOGGER.info('Simulation started at %d, ended at %d Effective simulation time was %d ms. ' %(simulation_start_ts, simulation_end_ts, simulation_time_ms))
+    simulation_time_ms = round((max_tss - min_tss) / 1000000)
+    G_LOGGER.info(f'Simulation started at {min_tss}, ended at {max_tss}. '
+                  f'Effective simulation time was {simulation_time_ms} ms.')
     
     # Compute message delivery
     total_messages = len(injected_msgs_dict)
@@ -320,7 +372,7 @@ def main():
             G_LOGGER.warning('Several publishers of message %s')
         
         published_ts = int(msg_data['published'][0]['ts'])
-        node_id = msg_data['published'][0]['node_id']
+        peer_id = msg_data['published'][0]['peer_id']
         
         pbar.set_description('Computing latencies of message %s' %msg_id)
 
@@ -328,13 +380,13 @@ def main():
         latencies = []
         for received_data in msg_data['received']:
             # Skip self
-            if received_data['node_id'] == node_id:
+            if received_data['peer_id'] == peer_id:
                 continue
             # NOTE: We are getting some negative latencies meaning that the message appears to be received before it was sent ...
             # I assume this must be because those are the nodes that got the message injected in the first place
             #  TLDR: Should be safe to ignore all the negative latencies
             latency = int(received_data['ts']) - published_ts
-            node_id = msg_data['published'][0]['node_id']
+            peer_id = msg_data['published'][0]['peer_id']
             latencies.append(latency)
                     
         msgs_dict[msg_id]['latencies'] = latencies
@@ -350,8 +402,8 @@ def main():
     memory_usage = []
     pbar = tqdm(node_logs.items())
     for node in pbar:
-        pbar.set_description('Fetching hardware stats from container %s' %node[1]['container_id'])
-        container_stats = fetch_cadvisor_stats_from_container(node[1]['container_id'], simulation_start_ts, simulation_end_ts)
+        pbar.set_description('Fetching hardware stats from container %s' %node[1]['container_name'])
+        container_stats = fetch_cadvisor_stats_from_container(node[1]['container_name'], min_tss, max_tss)
         # NOTE: Here we could also chose a different statistic such as mean or average instead of max
         cpu_usage.append(max(container_stats['cpu_usage']))
         memory_usage.append(max(container_stats['memory_usage']))
