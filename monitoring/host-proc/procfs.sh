c069923bd0a6abd33df9d090ef1649632eef2a8f
0xFugue <119708655+0xFugue@users.noreply.github.com>
2023-04-20 00:16:02 +0530

    Data collection: docker stats and host-proc (#106)
    * sysfs, docker stats, docker ps
    
    * .gitignore
    
    * updated file names
    
    * cpu, mem, i/o
    
    * .gitignore
    
    * .gitignore
    
    * added scheduler
    
    * pruned mem/blk measurement
    
    * cpu, mem, blk, net stats
    
    * single node containers one
    
    * renaming,bug fix,  signal handling
    
    * code clean up; added all the necessary xlations to the logs
    
    * cpustat, pid2docker
    
    * remove temp. files
    
    * tear_down, permission exception
    
    * updating .gitignore
    
    * param validation, sudo blk reads
    
    * added output dir
    
    * comments
    
    * .gitignore & rename
    
    * .gitignore & rename
    
    * de-escalate privileges
    
    * proc monitor
    
    * container id to host end of  virtual interface
    
    * complete code rewrite with minimal sudo section
    
    * set default iface for non-docker wakus
    
    * added signal handlers and clean shutdown
    
    * added 3 different per-process network stats
    
    * +VmData, +VmStk, handling of veth pairs, optimal cpu stats
    
    * +VmHWM, net1=net3, net2
    
    * +VmHWM, net1=net3, net2
    
    * pre merge
    
    * pre merge
    
    * .gitignore
    
    * adjusted paths & stats only nim/go waku
    
    * monitor.sh
    
    * adding docker wait, nim-waku:nwaku-trace2
    
    * TTY bug
    
    * copy config/metric dirs to logs
    
    * cleanup.sh
    
    * procfs monitor updates
    
    * run.sh cleanup
    
    * run.sh  metrics_infra cadvisor
    
    * cleanup fix
    
    * empty output dir
    
    * host-proc
    
    * added cadvisor conditionals
    
    * enable WLS wait
    
    * signal WLS once the measuring infra is up
    
    * spin_up wls_cid
    
    * remove print/exit
    
    * renaming
    
    * graceful signal exit, reduce logging freq
    
    * stat only running waku instances
    
    * comments, remove prints
    
    * de-duplicate docker calls
    
    * deduplicate docker calls in procfs.py
    
    * multi nodes
    
    * adding Jordi's signal'ng code and timing
    
    * minimise the setup time
    
    * split and move sudo ahead
    
    * run.sh/procfs/docker stats named pipe synchornisation
    
    * jack-up the max# open filehandles
    
    * 100k file handles
    
    * log timings, set resource limits
    
    * complete rewrite of pid2did
    
    * added start time + waku versions
    
    * requirements.txt
    
    * assert, stat collection duration
    
    * main.sh -> dstats.sh

---
+++ monitoring/host-proc/procfs.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+rclist=$1
+odir=$2
+usr=$3
+grp=$4
+signal_fifo=$5
+
+sinterval=1
+proclog=$odir/docker-proc-log.out
+
+# blocks until signalled
+echo "host-proc: /proc fs monitor : waiting on $signal_fifo"
+cat  $signal_fifo
+
+export SIGNAL_FIFO=$signal_fifo
+# load the environ vars
+. $rclist
+
+#echo "export $DPS_FNAME $DINSPECT_FNAME $PIDLIST_FNAME $ID2VETH_FNAME $PROCOUT_FNAME $LOCAL_IF $WAIT_CID"
+
+
+# 10k * 10 /proc entries per process
+max_wakunodes=10000
+nfh=$((10*max_wakunodes))
+ulimit -n $nhf    # jack-up the number of files handles for all the children
+
+# add date and the names/versions of waku images present
+echo "# /procfs started: $(date)" > $PROCOUT_FNAME
+echo "# /procfs images involved: $(docker images | grep waku | tr '\n' '; ' )"  >> $PROCOUT_FNAME
+
+echo "host-proc: starting the /proc fs monitor"
+python3 ./procfs.py --sampling-interval $sinterval --prefix $odir --wls-cid $WAIT_CID  > $proclog 2>&1 &
+procfs_pid=$!
+echo "host-proc: waiting for WLS to finish: procfs $procfs_pid running"
+docker container wait $WAIT_CID   # now wait for the wakurtosis to finish
+sleep 60        # make sure you collect the stats until last messages settle down
+
+echo "host-proc: stopping /proc fs monitor $procfs_pid"
+kill -15  $procfs_pid   # procfs-stats is a su process
+
+echo "host-proc: updating the owner of logs: $usr, $grp"
+chown $usr:$grp $proclog $PROCOUT_FNAME
--- run.sh
+++ run.sh
@@ -1,118 +1,192 @@
 #!/bin/sh
 
+#TODO: make them functions
+##################### SETUP & CLEANUP
+if [ "$#" -eq 0 ]; then
+    echo "Error: Must select the measurement infra: cadvisor, host-proc, container-proc"
+    echo "Usage: sh ./run.sh <measurement_infra> [enclave_name] [config_file]"
+    exit 1
+fi
+
 dir=$(pwd)
 
-# Parse arg if any
-ARGS1=${1:-"wakurtosis"}
-ARGS2=${2:-"config.json"}
+# Parse args
+metrics_infra=${1:-"cadvisor"}
+enclave_name=${2:-"wakurtosis"}
+wakurtosis_config_file=${3:-"config.json"}
 
-# Main .json configuration file
-enclave_name=$ARGS1
-wakurtosis_config_file=$ARGS2
 loglevel="error"
-
+echo "- Metrics Infra: " $metrics_infra
 echo "- Enclave name: " $enclave_name
 echo "- Configuration file: " $wakurtosis_config_file
 
-# Delete the enclave just in case
-echo -e "\nCleaning up Kurtosis environment "$enclave_name
-docker container stop cadvisor > /dev/null 2>&1
-docker container rm cadvisor > /dev/null 2>&1
-kurtosis enclave rm -f $enclave_name > /dev/null 2>&1
-# kurtosis clean -a > /dev/null 2>&1 we do not want to delete all enclaves, just the one we will execute
-
-# Delete previous logs
-echo -e "\Deleting previous logs in ${enclave_name}_logs"
-rm -rf ./${enclave_name}_logs > /dev/null 2>&1
-rm ./kurtosisrun_log.txt > /dev/null 2>&1
-
-# Preparing enclave
-echo "Preparing enclave..."
-kurtosis enclave add --name ${enclave_name}
-enclave_preffix="$(kurtosis enclave inspect --full-uuids $enclave_name | grep UUID: | awk '{print $2}')"
-echo "Enclave network: "$enclave_preffix
-
-# Get enclave last IP
-subnet="$(docker network inspect $enclave_preffix | jq -r '.[].IPAM.Config[0].Subnet')"
-echo "Enclave subnetork: $subnet"
-last_ip="$(ipcalc $subnet | grep HostMax | awk '{print $2}')"
-echo "cAdvisor IP: $last_ip"
+# Cleanup previous runs
+echo -e "\Cleaning up previous runs"
+sh ./cleanup.sh $enclave_name
+echo -e "\Done cleaning up previous runs"
+##################### END
 
 
-# Set up Cadvisor
-# docker run --volume=/:/rootfs:ro --volume=/var/run:/var/run:rw --volume=/var/lib/docker/:/var/lib/docker:ro --volume=/dev/disk/:/dev/disk:ro --volume=/sys:/sys:ro --volume=/etc/machine-id:/etc/machine-id:ro --publish=8080:8080 --detach=true --name=cadvisor --privileged --device=/dev/kmsg gcr.io/cadvisor/cadvisor
-docker run --volume=/:/rootfs:ro --volume=/var/run:/var/run:rw --volume=/var/lib/docker/:/var/lib/docker:ro --volume=/dev/disk/:/dev/disk:ro --volume=/sys:/sys:ro --volume=/etc/machine-id:/etc/machine-id:ro --publish=8080:8080 --detach=true --name=cadvisor --privileged --device=/dev/kmsg --network $enclave_preffix --ip=$last_ip gcr.io/cadvisor/cadvisor:v0.47.0
+##################### CADVISOR
+if [ "$metrics_infra" = "cadvisor" ];
+then
+    # Preparing enclave
+    echo "Preparing the enclave..."
+    kurtosis  --cli-log-level $loglevel  enclave add --name ${enclave_name}
+    enclave_prefix=$(kurtosis --cli-log-level $loglevel  enclave inspect --full-uuids $enclave_name | grep UUID: | awk '{print $2}')
+    echo "Enclave network: "$enclave_prefix
+
+    # Get enclave last IP
+    subnet="$(docker network inspect $enclave_prefix | jq -r '.[].IPAM.Config[0].Subnet')"
+    echo "Enclave subnetork: $subnet"
+    last_ip="$(ipcalc $subnet | grep HostMax | awk '{print $2}')"
+    echo "cAdvisor IP: $last_ip"
+
+    # Set up Cadvisor
+    docker run --volume=/:/rootfs:ro --volume=/var/run:/var/run:rw --volume=/var/lib/docker/:/var/lib/docker:ro --volume=/dev/disk/:/dev/disk:ro --volume=/sys:/sys:ro --volume=/etc/machine-id:/etc/machine-id:ro --publish=8080:8080 --detach=true --name=cadvisor --privileged --device=/dev/kmsg --network $enclave_prefix --ip=$last_ip gcr.io/cadvisor/cadvisor:v0.47.0
+    # docker run --volume=/:/rootfs:ro --volume=/var/run:/var/run:rw --volume=/var/lib/docker/:/var/lib/docker:ro --volume=/dev/disk/:/dev/disk:ro --volume=/sys:/sys:ro --volume=/etc/machine-id:/etc/machine-id:ro --publish=8080:8080 --detach=true --name=cadvisor --privileged --device=/dev/kmsg gcr.io/cadvisor/cadvisor
+fi
+##################### END
 
 
-# Delete topology
-rm -rf ./config/topology_generated > /dev/null 2>&1
-# Remove previous logs
-rm -rf ./$enclave_name_logs > /dev/null 2>&1
 
+##################### GENNET
 # Run Gennet docker container
 echo -e "\nRunning network generation"
-docker rm gennet-container > /dev/null 2>&1  # cleanup the old docker if any
-
-docker run --name gennet-container -v ${dir}/config/:/config:ro gennet --config-file /config/${wakurtosis_config_file} --traits-dir /config/traits
+docker run --name cgennet -v ${dir}/config/:/config:ro gennet --config-file /config/${wakurtosis_config_file} --traits-dir /config/traits
 err=$?
-
 if [ $err != 0 ]
 then
   echo "Gennet failed with error code $err"
   exit
 fi
+# Copy the network generated TODO: remove this extra copy
+docker cp cgennet:/gennet/network_data ${dir}/config/topology_generated
+docker rm cgennet > /dev/null 2>&1
+##################### END
 
-docker cp gennet-container:/gennet/network_data ${dir}/config/topology_generated
 
-docker rm gennet-container > /dev/null 2>&1
 
+##################### HOST PROCFS MONITOR : PROLOGUE
+usr=`id -u`
+grp=`id -g`
+odir=./stats
+signal_fifo=/tmp/hostproc-signal.fifo   # do not create fifo under ./stats, or inside the repo
+if [ "$metrics_infra" = "host-proc" ];
+then
+    rclist=$odir/docker-rc-list.out
+    cd monitoring/host-proc
+    mkdir -p $odir
+    mkfifo $signal_fifo
+    chmod 0777 $signal_fifo
+    sudo sh ./procfs.sh $rclist $odir $usr $grp $signal_fifo &
+    cd -
+fi
+##################### END
+
+
+
+##################### KURTOSIS RUN
 # Create the new enclave and run the simulation
 jobs=$(cat config/${wakurtosis_config_file} | jq -r ".kurtosis.jobs")
-
 echo -e "\nSetting up the enclave: $enclave_name"
 kurtosis_cmd="kurtosis --cli-log-level \"$loglevel\" run --enclave-id ${enclave_name} . '{\"wakurtosis_config_file\" : \"config/${wakurtosis_config_file}\"}' --parallelism ${jobs} > kurtosisrun_log.txt 2>&1"
-
 START=$(date +%s)
-eval $kurtosis_cmd
+  eval $kurtosis_cmd
 END1=$(date +%s)
+
 DIFF1=$(( $END1 - $START ))
 echo -e "Enclave $enclave_name is up and running: took $DIFF1 secs to setup"
 
 # Extract the WLS service name
 wls_service_name=$(kurtosis --cli-log-level $loglevel enclave inspect $enclave_name | grep wls | awk '{print $1}')
 # kurtosis service logs $enclave_name $wls_service_name
-echo -e "\n--> To see simulation logs run: kurtosis service logs $enclave_name $wls_service_name <--"
+echo "\n--> To see simulation logs run: kurtosis service logs $enclave_name $wls_service_name <--"
+##################### END
 
 
-# Fetch the Grafana address & port
-grafana_host=$(kurtosis enclave inspect $enclave_name | grep grafana | awk '{print $6}')
-echo -e "\n--> Statistics in Grafana server at http://$grafana_host/ <--"
 
-echo "Output of kurtosis run command written in kurtosisrun_log.txt"
-
-### Wait for WLS to finish
-
-# Get the container prefix/uffix for the WLS service
-service_name="$(kurtosis --cli-log-level $loglevel  enclave inspect $enclave_name | grep $wls_service_name | awk '{print $2}')"
-service_uuid="$(kurtosis --cli-log-level $loglevel  enclave inspect --full-uuids $enclave_name | grep $wls_service_name | awk '{print $1}')"
+##################### EXTRACT WLS CID
+# Get the container prefix/suffix for the WLS service
+service_name=$(kurtosis --cli-log-level $loglevel  enclave inspect $enclave_name | grep $wls_service_name | awk '{print $2}')
+service_uuid=$(kurtosis --cli-log-level $loglevel  enclave inspect --full-uuids $enclave_name | grep $wls_service_name | awk '{print $1}')
 
 # Construct the fully qualified container name that kurtosis has created
-cid="$service_name--$service_uuid"
+wls_cid="$service_name--$service_uuid"
+##################### END
 
+
+
+##################### HOST PROCFS MONITOR: EPILOGUE
+if [ "$metrics_infra" = "host-proc" ];
+then
+    echo "Starting the /proc fs and docker stat measurements"
+    cd monitoring/host-proc
+    sh ./dstats.sh  $wls_cid $odir $signal_fifo &
+    cd -
+fi
+##################### END
+
+
+
+##################### DOCKER PROCFS MONITOR
+if [ "$metrics_infra" = "container-proc" ];
+then
+    echo "Jordi's measurement infra goes here"
+fi
+##################### END
+
+
+##################### GRAFANA
+# Fetch the Grafana address & port
+grafana_host=$(kurtosis --cli-log-level $loglevel  enclave inspect $enclave_name | grep grafana | awk '{print $6}')
+echo -e "\n--> Statistics in Grafana server at http://$grafana_host/ <--"
+echo "Output of kurtosis run command written in kurtosisrun_log.txt"
+##################### END
+
+
+
+##################### WAIT FOR THE WLS TO FINISH
 # Wait for the container to halt; this will block
 echo -e "Waiting for simulation to finish ..."
-status_code="$(docker container wait $cid)"
-
-### Logs
-kurtosis enclave dump ${enclave_name} ${enclave_name}_logs > /dev/null 2>&1
+status_code="$(docker container wait $wls_cid)"
 echo -e "Simulation ended with code $status_code Results in ./${enclave_name}_logs"
-
 END2=$(date +%s)
 DIFF2=$(( $END2 - $END1 ))
-
 echo "Simulation took $DIFF1 + $DIFF2 = $(( $END2 - $START)) secs"
+##################### END
+
+# give time for the messages to settle down before we collect logs
+sleep 60
+
+##################### GATHER CONFIG, LOGS & METRICS
+# dump logs
+echo "Dumping Kurtosis logs"
+kurtosis enclave dump ${enclave_name} ${enclave_name}_logs > /dev/null 2>&1
+cp kurtosisrun_log.txt ${enclave_name}_logs
+
+# copy metrics data, config, network_data to the logs dir
+cp -r ./config ${enclave_name}_logs
+
+if [ "$metrics_infra" = "host-proc" ];
+then
+    echo "Copying the /proc fs and docker stat measurements"
+    cp -r ./monitoring/host-proc/stats  ${enclave_name}_logs/host-proc-stats
+fi
+
+if [ "$metrics_infra" = "container-proc" ];
+then
+    echo "Jordi's data copy goes here"
+fi
+
 # Copy simulation results
-# docker cp "$cid:/wls/summary.json" "./${enclave_name}_logs" > /dev/null 2>&1
-docker cp "$cid:/wls/messages.json" "./${enclave_name}_logs"
+# docker cp "$wls_cid:/wls/summary.json" "./${enclave_name}_logs" > /dev/null 2>&1
+docker cp "$wls_cid:/wls/messages.json" "./${enclave_name}_logs"
+
+echo "- Metrics Infra:  $metrics_infra" > ./${enclave_name}_logs/run_args
+echo "- Enclave name:  $enclave_name" >> ./${enclave_name}_logs/run_args
+echo "- Configuration file:  $wakurtosis_config_file" >> ./${enclave_name}_logs/run_args
+
 
 echo "Done."
+##################### END
