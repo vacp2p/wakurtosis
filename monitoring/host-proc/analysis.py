2a0f3b2289a09e0eb939f015d84e9e0326e8f1eb
0xFugue <119708655+0xFugue@users.noreply.github.com>
2023-04-26 00:01:20 +0530

    faster Human2ByteConveter

--- monitoring/host-proc/analysis.py
+++ monitoring/host-proc/analysis.py
@@ -2,18 +2,17 @@ import typer
 
 import sys
 import os
-#from stat import *
 import stat
 from pathlib import Path
 
 import time
-import re
 
-import matplotlib.pyplot as plt
+import re
 
 import logging as log
 import pandas as pd
 import numpy as np
+import matplotlib.pyplot as plt
 
 
 # check if the path exists and is of appropriate type
@@ -32,6 +31,7 @@ def path_ok(path : Path, isDir=False):
     return True
 
 
+# define singleton 
 class Singleton(type):
     _instances = {}
     def __call__(cls, *args, **kwargs):
@@ -41,35 +41,29 @@ class Singleton(type):
 
 
 # convert human readable sizes to bytes
-class SizeConverter(metaclass=Singleton):
-    def __init__(self):
-        self.let3 =  {'GiB':1024*1024*1024, 'MiB':1024*1024, 'KiB':1024}
-        self.let22 = {'GB':1024*1024*1024, 'MB':1024*1024, 'KB':1024}
-        self.let21 = {'gB':1000*1000*1000, 'mB':1000*1000, 'kB':1000}
+class Human2ByteConverter(metaclass=Singleton):
+    def __init__(self):     # add any human readable format/size and multiplier here
+        self.letters    = {}
+        self.letters[3] = {'GiB' : 1024*1024*1024, 'MiB' : 1024*1024, 'KiB' : 1024}
+        self.letters[2] = {'GB' : 1024*1024*1024, 'MB' : 1024*1024, 'KB' : 1024,
+                            'gB' : 1000*1000*1000, 'mB' : 1000*1000, 'kB' : 1000}
+        self.letters[1] = {'B':1}
 
     def convert(self, value):
-        k3, k2, k1 = value[-3:], value[-2:], value[-1:]
-        if k3 in self.let3:
-            return float(value[:-3]) * self.let3[k3]
-        elif k2 in self.let22:
-            return float(value[:-2]) * self.let22[k2]
-        elif k2 in self.let21:
-            return float(value[:-2]) * self.let21[k2]
-        elif k1 == 'B':
-            return float(value[:-1])
-        else:
-            return np.nan
+        for i in [3, 2, 1]:
+            k = value[-i:]
+            if k in self.letters[i]:
+                return float(value[:-i]) * self.letters[i][k]
+        return np.nan
 
 
 # handle docker stats
 class DStats:
     def __init__(self, fname):
-        self.fname = fname
-        self.df = ""
-        self.waku_cids = []
-        self.n = 0
+        self.fname, self.df, self.waku_cids, self.n = fname, "", [], 0
+        self.start_processing()
 
-    # remove formatting artefacts
+    # remove the formatting artefacts
     def pre_process(self):
         regex = re.compile(r'\x1B(?:[@-Z\\-_]|\[[0-?]*[ -/]*[@-~])')
         with open(self.fname) as f:
@@ -79,22 +73,25 @@ class DStats:
 
     # make sure the df is all numeric
     def post_process(self):
-        sc = SizeConverter()
+        h2b = Human2ByteConverter()
         for percent in ["CPUPerc", "MemPerc"]:
             self.df[percent] = self.df[percent].str.replace('%','').astype(float)
         for size in ["MemUse", "MemTotal", "NetRecv", "NetSent", "BlockR", "BlockW"]:
-            self.df[size] = self.df[size].map(lambda x: sc.convert(x.strip()))
+            self.df[size] = self.df[size].map(lambda x: h2b.convert(x.strip()))
         self.waku_cids = self.df["ContainerID"].unique()
         self.n = len(self.waku_cids)
-        return self.df
 
-    def get_df(self):
+    # build df from csv
+    def start_processing(self):
         self.pre_process()
         self.df = pd.read_csv(self.fname, header=0,  comment='#', skipinitialspace = True,
                                 delimiter='/', usecols=["ContainerID", "ContainerName",
                                     "CPUPerc", "MemUse", "MemTotal", "MemPerc",
                                     "NetRecv", "NetSent", "BlockR","BlockW",  "PIDS"])
-        return self.post_process()
+        self.post_process()
+
+    def get_df(self):
+        return self.df
 
 
 # instantiate typer and set the commands
@@ -118,7 +115,8 @@ def dstats(dstats_fname: Path):
     if not path_ok(dstats_fname):
         sys.exit(0)
 
-    df = DStats(dstats_fname).get_df()
+    dstats = DStats(dstats_fname)
+    df = dstats.get_df()
 
     df["CPUPerc"].plot()
     df["MemPerc"].plot()
