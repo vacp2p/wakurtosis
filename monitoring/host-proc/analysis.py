346e5da0857f01255f732aa0cd8d79d523775f63
0xFugue <119708655+0xFugue@users.noreply.github.com>
2023-04-25 13:31:09 +0530

    cli scaffolding for processings metrics

---
+++ monitoring/host-proc/analysis.py
@@ -0,0 +1,53 @@
+import typer
+
+import sys
+import os
+#from stat import *
+import stat
+from pathlib import Path
+
+import logging as log
+
+
+# check if the path exists and is of appropriate type
+def path_ok(path : Path, isDir=False):
+    if not path.exists():
+        log.error(f'"{path}" does not exist')
+        return False
+    mode = path.stat().st_mode
+    if not isDir and not stat.S_ISREG(mode):
+        log.error(f'File expected: "{path}" is not a file')
+        return False
+    if isDir and not stat.S_ISDIR(mode):
+        log.error(f'Directory expected: "{path}" is not a directory')
+        return False
+    # lay off permission checks; resolve them lazily with open
+    return True
+
+# instantiate typer and set the commands
+app = typer.Typer()
+
+# process / plot docker-procfs.out
+@app.command()
+def procfs(procfs_fname: Path):
+    if not path_ok(procfs_fname):
+        sys.exit(0)
+    print(f'Got {procfs_fname}')
+
+# process / plot docker-dstats.out
+@app.command()
+def dstats(dstats_fname: Path):
+    if not path_ok(dstats_fname):
+        sys.exit(0)
+    print(f'Got {dstats_fname}')
+
+# add jordi's log processing for settling time
+@app.command()
+def simlog(log_dir: Path):
+    if not path_ok(log_dir, True):
+        sys.exit(0)
+    print(f'Got {log_dir}')
+
+
+if __name__ == "__main__":
+    app()
