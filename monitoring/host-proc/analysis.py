9b9f63192a7122ef6b58ad7664426ea60557cb9f
0xFugue <119708655+0xFugue@users.noreply.github.com>
2023-05-02 22:44:44 +0530

    add jordis propagation delay calc - partial

--- monitoring/host-proc/analysis.py
+++ monitoring/host-proc/analysis.py
@@ -15,7 +15,18 @@ import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib.backends.backend_pdf import PdfPages
 
+from tqdm_loggable.auto import tqdm
 import seaborn as sns
+
+from src import vars
+from src import arg_parser
+from src import topology
+from src import log_parser
+from src import analysis
+#from src import prometheus
+from src import analysis_logger
+from src import plotting
+
 
 # check if the path exists and is of appropriate type
 def path_ok(path : Path, isDir=False):
@@ -43,7 +54,7 @@ class Singleton(type):
 
 
 # convert human readable sizes to bytes
-class Human2ByteConverter(metaclass=Singleton):
+class Human2BytesConverter(metaclass=Singleton):
     def __init__(self):     # add any human readable format/size and multiplier here
         self.letters    = {}
         self.letters[3] = {'GiB' : 1024*1024*1024, 'MiB' : 1024*1024, 'KiB' : 1024}
@@ -100,7 +111,7 @@ class DStats:
         for name in ["ContainerID", "ContainerName"]:
             self.df[name] = self.df[name].map(lambda x: x.strip())
         self.waku_cids = self.df["ContainerID"].unique()
-        h2b, n = Human2ByteConverter(), len(self.waku_cids)
+        h2b, n = Human2BytesConverter(), len(self.waku_cids)
         for percent in ["CPUPerc", "MemPerc"]:
             self.df[percent] = self.df[percent].str.replace('%','').astype(float)
         for size in ["MemUse", "MemTotal"]:
@@ -183,6 +194,35 @@ class DStats:
         return self.df
 
 
+# add jordi's log processing for settling time
+def compute_settling_time(log_dir: Path):
+    #if not path_ok(log_dir, True):
+    #    sys.exit(0)
+
+    """ Parse args """
+    simulation_path, tomls_folder = log_dir, f'{log_dir}/config/topology_generated/'
+
+    """ Load Topics Structure """
+    topology_info = topology.load_topology(f'{log_dir}/{vars.G_TOPOLOGY_FILE_NAME}')
+    topology.load_topics_into_topology(topology_info, tomls_folder)
+
+    """ Load Simulation Messages """
+    injected_msgs_dict = log_parser.load_messages(simulation_path)
+    print(topology_info, simulation_path)
+    node_logs, msgs_dict, min_tss, max_tss = analysis.analyze_containers(topology_info,
+                                                                         simulation_path)
+
+    """ Compute simulation time window """
+    simulation_time_ms = round((max_tss - min_tss) / 1000000)
+    analysis_logger.G_LOGGER.info(f'Simulation started at {min_tss}, ended at {max_tss}. '
+                                  f'Effective simulation time was {simulation_time_ms} ms.')
+
+    analysis.compute_message_delivery(msgs_dict, injected_msgs_dict)
+    analysis.compute_message_latencies(msgs_dict)
+    msg_propagation_times = analysis.compute_propagation_times(msgs_dict)
+    msg_injection_times = analysis.compute_injection_times(injected_msgs_dict)
+    print(f'Got {log_dir}')
+
 # instantiate typer and set the commands
 app = typer.Typer()
 
@@ -200,25 +240,19 @@ def procfs(procfs_fname: Path):
 
 # process / plot docker-dstats.out
 @app.command()
-def dstats(dstats_fname: Path,
-            prefix:str = typer.Option("out", help="Specify the prefix for the plots"),
+def dstats(dstats_fname: Path, log_dir: str,
+            prefix:str = typer.Option("out", help="Specify the prefix for the plot pdfs"),
             cdf: bool = typer.Option(True, help="Specify the prefix for the plots")):
     if not path_ok(dstats_fname):
         sys.exit(0)
 
     dstats = DStats(dstats_fname, prefix)
-    dstats.violin_plots(cdf)
-
+    #dstats.violin_plots(cdf)
+    compute_settling_time(log_dir)
     df = dstats.get_df()
 
     print(f'Got {dstats_fname}')
 
-# add jordi's log processing for settling time
-@app.command()
-def simlog(log_dir: Path):
-    if not path_ok(log_dir, True):
-        sys.exit(0)
-    print(f'Got {log_dir}')
 
 
 if __name__ == "__main__":
