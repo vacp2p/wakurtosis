1cb8489fdc52c5a53e057af11dff2b4c8e17c4a1
Alberto <alberto.soutullo639@gmail.com>
2023-05-05 18:18:30 +0200

    Deleted toml folder from arg parser

--- analysis-module/src/arg_parser.py
+++ analysis-module/src/arg_parser.py
@@ -10,14 +10,11 @@ def parse_args():
     parser = argparse.ArgumentParser()
     parser.add_argument("-sp", "--simulation_path", help="Simulation results path",
                         default=vars.G_DEFAULT_SIMULATION_PATH)
-    parser.add_argument("-t", "--toml_folder", help="Tomls folder name",
-                        default=vars.G_DEFAULT_TOML_PATH)
     parser.add_argument("-p", "--prometheus_port", help="Pometheus port")
 
     args = parser.parse_args()
 
     simulation_path = args.simulation_path
-    tomls_folder = args.toml_folder
     port = args.prometheus_port
 
-    return simulation_path, tomls_folder, port
+    return simulation_path, port
