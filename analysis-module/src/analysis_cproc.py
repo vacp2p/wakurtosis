96638c8745a70aa5530c5c67806f119d237d23c3
Daimakaimura <17453177+Daimakaimura@users.noreply.github.com>
2023-05-11 09:42:32 +0100

    Refactoring

--- analysis-module/src/analysis_cproc.py
+++ analysis-module/src/analysis_cproc.py
@@ -3,6 +3,7 @@ import re
 import sys
 import json
 from datetime import datetime
+from typing import Any, Dict, List, Set, Optional, Tuple
 
 # Project Imports
 from src import vars
@@ -17,7 +18,44 @@ def extract_node_id(s: str) -> str:
         return None
 
 
-def load_process_level_metrics(metrics_file_path: str):
+def parse_container_nodes(container_id: str, container_data: Dict[str, Any], container_nodes: Dict[int, Any], metrics_dict: Dict[Any, Dict[str, Any]]) -> None:    
+    nodes_cnt = 0
+
+    for sample in container_data['samples']:
+        
+        if sample['PID'] not in container_nodes:
+            analysis_logger.G_LOGGER.error('Couldn\'t find node id for PID %d in container %s' %(sample['PID'], container_id))
+            continue
+        
+        node_id = container_nodes[sample['PID']]
+        
+        if not node_id:
+            analysis_logger.G_LOGGER.error('Couldn\'t find node id for PID %d in container %s' %(sample['PID'], container_id))
+            continue
+
+        if node_id in metrics_dict:
+            metrics_dict[node_id]['samples'].append(sample)
+        else:
+            nodes_cnt += 1
+            metrics_dict[node_id] = {'samples' : [sample]}
+
+def extract_container_nodes(container_id: str, container_data: Dict[str, Any]) -> Dict[int, Any]:
+
+    container_nodes = {}
+    for process in container_data['info']['processes']:
+        
+        node_id = extract_node_id(process['binary'])
+        if not node_id:
+            analysis_logger.G_LOGGER.error('Couldn\'t match %s to node id in container %s' %(process['binary'], container_id))
+            continue
+        
+        pid = process['pid']
+        container_nodes[pid] = node_id  
+    
+    return container_nodes
+                
+
+def load_process_level_metrics(metrics_file_path: str) -> Tuple[Dict[str, Any], Optional[Dict[str, Any]]]:
     
     metrics_dict = {}
     
@@ -28,44 +66,16 @@ def load_process_level_metrics(metrics_file_path: str):
             
             info = metrics_obj['header']
             all_samples = metrics_obj['containers']
-            nodes_cnt = 0
-
+            
             if len(all_samples) != info['num_containers']:
-                G_LOGGER.error('Number of containers in header does not match number of containers in samples')
-                return {}, None
+                analysis_logger.G_LOGGER.error('Number of containers in header does not match number of containers in samples')
+                return metrics_dict, info
 
             for container_id, container_data in all_samples.items():
 
-                # tomls file names are unique per node
-                container_nodes = {}
-                for process in container_data['info']['processes']:
-                    
-                    node_id = extract_node_id(process['binary'])
-                    if not node_id:
-                        G_LOGGER.error('Couldn\'t match %s to node id in container %s' %(process['binary'], container_id))
-                        continue
-                    
-                    pid = process['pid']
-                    container_nodes[pid] = node_id  
+                container_nodes = extract_container_nodes(container_id, container_data)
+                parse_container_nodes(container_id, container_data, container_nodes, metrics_dict)
                 
-                # Parse samples for each node
-                for sample in container_data['samples']:
-                    
-                    if sample['PID'] not in container_nodes:
-                        G_LOGGER.error('Couldn\'t find node id for PID %d in container %s' %(sample['PID'], container_id))
-                        continue
-                    
-                    node_id = container_nodes[sample['PID']]
-                    if not node_id:
-                        G_LOGGER.error('Couldn\'t find node id for PID %d in container %s' %(sample['PID'], container_id))
-                        continue
-
-                    if node_id in metrics_dict:
-                        metrics_dict[node_id]['samples'].append(sample)
-                    else:
-                        nodes_cnt += 1
-                        metrics_dict[node_id] = {'samples' : [sample]}
-            
     except Exception as e:
         analysis_logger.G_LOGGER.error('%s: %s' % (e.__doc__, e))
         sys.exit()
@@ -78,8 +88,7 @@ def load_process_level_metrics(metrics_file_path: str):
     return metrics_dict, info
 
 
-def build_summary(config_obj, metrics_info, msgs_dict, node_logs, topics, min_tss, max_tss, avg_samples_per_node):
-
+def build_summary(config_obj: Dict[str, Any], metrics_info: Dict[str, Any], msgs_dict: Dict[str, Any], node_logs: Dict[str, Any], topics: Set[str], min_tss: float, max_tss: float, avg_samples_per_node: float) -> Dict[str, Any]:
     simulation_summary = {'general' : {}, 'nodes' : {}, 'messages' : {}, 'parameters' : {}}
     simulation_summary['general']['datetime'] = datetime.now().isoformat()
     simulation_summary['general']['num_messages'] = len(msgs_dict)
@@ -103,8 +112,24 @@ def build_summary(config_obj, metrics_info, msgs_dict, node_logs, topics, min_ts
         
     return simulation_summary
 
-def compute_process_level_metrics(simulation_path, config_obj):
+def compute_node_metrics(node_obj, num_samples, max_cpu_usage, max_memory_usage, total_network_usage, max_disk_usage) -> None:
+    
+    num_samples.append(len(node_obj['samples']))
+        
+    """ Peak values """
+    max_cpu_usage.append(max(obj['CPUPercentage'] for obj in node_obj['samples']))
+    max_memory_usage.append(max(obj['MemoryUsageMB'] for obj in node_obj['samples']))
+    
+    """ Accumulated """
+    total_network_usage['rx_mbytes'].append(max(obj['NetStats']['all']['total_received'] for obj in node_obj['samples']) / (1024*1024))
+    total_network_usage['tx_mbytes'].append(max(obj['NetStats']['all']['total_sent'] for obj in node_obj['samples']) / (1024*1024))
 
+    """ Accumulated """
+    max_disk_usage['disk_read_mbytes'].append(max(obj['DiskIORChar'] for obj in node_obj['samples']) / (1024*1024))
+    max_disk_usage['disk_write_mbytes'].append(max(obj['DiskIOWChar'] for obj in node_obj['samples']) / (1024*1024)) 
+
+def compute_process_level_metrics(simulation_path: str, config_obj: Dict[str, Any]) -> Tuple[Dict[str, Any], List[float], List[float], Dict[str, List[float]], Dict[str, List[float]], float]:
+    
     """ Load Metrics """
     metrics_file_path = f'{simulation_path}/cproc_metrics.json'
     node_metrics, metrics_info = load_process_level_metrics(metrics_file_path)
@@ -116,28 +141,16 @@ def compute_process_level_metrics(simulation_path, config_obj):
     max_disk_usage = {'disk_read_mbytes' : [], 'disk_write_mbytes' : []}
     num_samples = []
 
-    for node_id, node_obj in node_metrics.items():
-
-        num_samples.append(len(node_obj['samples']))
+    for _, node_obj in node_metrics.items():
+        compute_node_metrics(node_obj, num_samples, max_cpu_usage, max_memory_usage, total_network_usage, max_disk_usage)
         
-        # Peak values
-        max_cpu_usage.append(max(obj['CPUPercentage'] for obj in node_obj['samples']))
-        max_memory_usage.append(max(obj['MemoryUsageMB'] for obj in node_obj['samples']))
-        
-        # Accumulated 
-        total_network_usage['rx_mbytes'].append(max(obj['NetStats']['all']['total_received'] for obj in node_obj['samples']) / (1024*1024))
-        total_network_usage['tx_mbytes'].append(max(obj['NetStats']['all']['total_sent'] for obj in node_obj['samples']) / (1024*1024))
-
-        # Accumulated
-        max_disk_usage['disk_read_mbytes'].append(max(obj['DiskIORChar'] for obj in node_obj['samples']) / (1024*1024))
-        max_disk_usage['disk_write_mbytes'].append(max(obj['DiskIOWChar'] for obj in node_obj['samples']) / (1024*1024))
     
     avg_samples_per_node = sum(num_samples) / len(num_samples)
    
     return metrics_info, max_cpu_usage, max_memory_usage, total_network_usage, max_disk_usage, avg_samples_per_node
 
 
-def export_sumary(simulation_path, summary):
+def export_sumary(simulation_path: str, summary: Dict[str, Any]) -> None:
     
     summary_path = f'{simulation_path}/sumary.json'
 
