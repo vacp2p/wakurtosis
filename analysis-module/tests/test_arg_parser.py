fe823290f4a6e056fbf111d0d09ca61eace40de5
Daimakaimura <17453177+Daimakaimura@users.noreply.github.com>
2023-05-11 07:53:59 +0100

    Updated test_parse_args to test all args

--- analysis-module/tests/test_arg_parser.py
+++ analysis-module/tests/test_arg_parser.py
@@ -8,8 +8,10 @@ from src import arg_parser
 
 class TestAnalysis(unittest.TestCase):
 
-    @patch('sys.argv', ['my_script.py', '-sp', 'test1', '-t', 'test2'])
+    @patch('sys.argv', ['my_script.py', '-sp', 'test1', '-t', 'test2', '-p', 'test3', '-i', 'test4'])
     def test_parse_args(self):
-        sim, tomls = arg_parser.parse_args()
+        sim, tomls, port, infra = arg_parser.parse_args()
         self.assertEqual(sim, "test1")
         self.assertEqual(tomls, "test2")
+        self.assertEqual(port, "test3")
+        self.assertEqual(infra, "test4")
