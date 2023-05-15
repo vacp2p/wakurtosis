ce33e9ec3b2eff8344970df063f0c82ebaab00a9
Alberto Soutullo <alberto.soutullo639@gmail.com>
2023-05-15 17:36:47 +0200

    Changed prometheus to deal with changes in `config.json`

--- analysis-module/src/prometheus.py
+++ analysis-module/src/prometheus.py
@@ -6,6 +6,7 @@ from prometheus_api_client import PrometheusConnect
 
 # Project Imports
 from src import analysis_logger
+from src import plotting_configurations
 
 
 def connect_to_prometheus(port):
@@ -28,29 +29,36 @@ def get_hardware_metrics(metrics, topology, min_tss, max_tss, prom_port):
     for container_ip in pbar:
         pbar.set_description(f'Fetching hardware stats from container {container_ip}')
         try:
-            fetch_cadvisor_stats_from_prometheus_by_node(metrics, prometheus, container_ip, min_tss, max_tss)
+            fetch_cadvisor_stats_from_prometheus_by_node(metrics, prometheus, container_ip, min_tss,
+                                                         max_tss)
         except Exception as e:
             analysis_logger.G_LOGGER.error('%s: %s' % (e.__doc__, e))
             continue
 
-    fetch_cadvisor_stats_from_prometheus_by_simulation(metrics, prometheus, container_ips, min_tss, max_tss)
+    fetch_cadvisor_stats_from_prometheus_by_simulation(metrics, prometheus, container_ips, min_tss,
+                                                       max_tss)
 
 
-def fetch_cadvisor_stats_from_prometheus_by_simulation(metrics, prom, container_ips, start_ts, end_ts):
+def fetch_cadvisor_stats_from_prometheus_by_simulation(metrics, prom, container_ips, start_ts,
+                                                       end_ts):
     start_timestamp = datetime.utcfromtimestamp(start_ts / 1e9)
     end_timestamp = datetime.fromtimestamp(end_ts / 1e9)
 
     for metric in metrics["by_simulation"].values():
-        if type(metric["metric_name"]) is list:
-            metric["values"] = []
-            for i, submetric in enumerate(metric["metric_name"]):
-                values = fetch_accumulated_metric_for_all_nodes(prom, submetric, container_ips, start_timestamp,
-                                                                end_timestamp, metric["toMB"])
-                metric["values"].append(values)
+        plotting_config = plotting_configurations.plotting_config[metric]
+
+        if type(metric) is list:
+            plotting_config["values"] = []
+            for i, submetric in enumerate(metric):
+                values = fetch_accumulated_metric_for_all_nodes(prom, submetric, container_ips,
+                                                                start_timestamp,
+                                                                end_timestamp, plotting_config["toMB"])
+                plotting_config["values"].append(values)
         else:
-            metric.setdefault("values", []).append(
-                fetch_accumulated_metric_for_all_nodes(prom, metric["metric_name"], container_ips, start_timestamp,
-                                                       end_timestamp, metric["toMB"]))
+            plotting_config.setdefault("values", []).append(
+                fetch_accumulated_metric_for_all_nodes(prom, metric, container_ips,
+                                                       start_timestamp,
+                                                       end_timestamp, plotting_config["toMB"]))
 
 
 def fetch_cadvisor_stats_from_prometheus_by_node(metrics, prom, container_ip, start_ts, end_ts):
@@ -59,22 +67,24 @@ def fetch_cadvisor_stats_from_prometheus_by_node(metrics, prom, container_ip, st
     start_timestamp = datetime.utcfromtimestamp(start_ts / 1e9)
     end_timestamp = datetime.fromtimestamp(end_ts / 1e9)
 
-    for metric in metrics["by_node"].values():
-        stat_function = function_dispatcher[metric["statistic"]]
-        if type(metric["metric_name"]) is list:
+    for metric in metrics["by_node"]:
+        plotting_config = plotting_configurations.plotting_config[metric]
+        stat_function = function_dispatcher[plotting_config["statistic"]]
+        if type(metric) is list:
             if "values" not in metric.keys():
-                metric["values"] = [[] for _ in range(len(metric["metric_name"]))]
-            for i, submetric in enumerate(metric["metric_name"]):
+                plotting_config["values"] = [[] for _ in range(len(metric))]
+            for i, submetric in enumerate(metric):
                 values = fetch_metric(prom, submetric, container_ip, start_timestamp, end_timestamp,
-                                      metric["toMB"])
-                metric["values"][i].append(stat_function(values))
+                                      plotting_config["toMB"])
+                plotting_config["values"][i].append(stat_function(values))
         else:
-            values = fetch_metric(prom, metric["metric_name"], container_ip, start_timestamp, end_timestamp,
-                                  metric["toMB"])
-            metric.setdefault("values", []).append(stat_function(values))
+            values = fetch_metric(prom, metric, container_ip, start_timestamp, end_timestamp,
+                                  plotting_config["toMB"])
+            plotting_config.setdefault("values", []).append(stat_function(values))
 
 
-def fetch_accumulated_metric_for_all_nodes(prom, metric, container_ips, start_timestamp, end_timestamp,
+def fetch_accumulated_metric_for_all_nodes(prom, metric, container_ips, start_timestamp,
+                                           end_timestamp,
                                            to_mbytes=False):
     result = {}
     for ip in container_ips:
@@ -95,12 +105,14 @@ def fetch_accumulated_metric_for_all_nodes(prom, metric, container_ips, start_ti
 
 
 def fetch_metric(prom, metric, ip, start_timestamp, end_timestamp, to_mbytes=False):
-    metric_result = prom.custom_query_range(f"{metric}{{container_label_com_kurtosistech_private_ip = '{ip}'}}",
-                                            start_time=start_timestamp, end_time=end_timestamp, step="1s")
+    metric_result = prom.custom_query_range(
+        f"{metric}{{container_label_com_kurtosistech_private_ip = '{ip}'}}",
+        start_time=start_timestamp, end_time=end_timestamp, step="1s")
     if not metric_result:
         analysis_logger.G_LOGGER.error(f"{metric} returns no data. Adding zero.")
         return [0]
-    metric_values = [float(metric_result[0]['values'][i][1]) for i in range(len(metric_result[0]['values']))]
+    metric_values = [float(metric_result[0]['values'][i][1]) for i in
+                     range(len(metric_result[0]['values']))]
     if to_mbytes:
         metric_values = [value / (1024 * 1024) for value in metric_values]
 
@@ -108,8 +120,9 @@ def fetch_metric(prom, metric, ip, start_timestamp, end_timestamp, to_mbytes=Fal
 
 
 def fetch_metric_with_timestamp(prom, metric, ip, start_timestamp, end_timestamp):
-    metric_result = prom.custom_query_range(f"{metric}{{container_label_com_kurtosistech_private_ip = '{ip}'}}",
-                                            start_time=start_timestamp, end_time=end_timestamp, step="1s")
+    metric_result = prom.custom_query_range(
+        f"{metric}{{container_label_com_kurtosistech_private_ip = '{ip}'}}",
+        start_time=start_timestamp, end_time=end_timestamp, step="1s")
 
     if not metric_result:
         analysis_logger.G_LOGGER.error(f"{metric} returns no data. Adding zero.")
