608e6b4e6d1d3c0052b2d1680e35739ab2e1c55b
Daimakaimura <17453177+Daimakaimura@users.noreply.github.com>
2023-05-16 13:20:45 +0100

    Refactored main and added cproc and cadvisor helpers

--- analysis-module/src/main.py
+++ analysis-module/src/main.py
@@ -6,25 +6,21 @@ from src import arg_parser
 from src import topology
 from src import log_parser
 from src import analysis
-from src import prometheus
 from src import analysis_logger
-from src import plotting
-from src import analysis_cproc
-from src import config
+from src import cproc
+from src import cadvisor
 
 if __name__ == "__main__":
     """ Parse args """
     simulation_path, prom_port, infra_type = arg_parser.parse_args()
 
     """Load Configuration"""
-    config = topology.load_json(simulation_path + "config/config.json")
-    metrics = config["plotting"]
+    simulation_config = topology.load_json(simulation_path + "config/config.json")
+    metrics = simulation_config["plotting"]
 
     """ Load Topics Structure """
     topology_info = topology.load_json(simulation_path + vars.G_TOPOLOGY_FILE_NAME)
     topology.load_topics_into_topology(topology_info, simulation_path + "config/topology_generated/")
-
-    simulation_config = topology.load_json(simulation_path + "config/config.json")
 
     """ Load Simulation Messages """
     injected_msgs_dict = log_parser.load_messages(simulation_path)
@@ -41,33 +37,13 @@ if __name__ == "__main__":
     msg_propagation_times = analysis.compute_propagation_times(msgs_dict)
     msg_injection_times = analysis.compute_injection_times(injected_msgs_dict)
 
-    """ Generate stats depending on the type of measurements we have """
+    """ Generate stats depending on the type of measurements infrastructure """
     if infra_type == 'container-proc':
-        analysis_logger.G_LOGGER.info('Generating stats for container_proc infrastructure ...')
-
-        metrics_info, max_cpu_usage, max_memory_usage, total_network_usage, max_disk_usage, avg_samples_per_node = analysis_cproc.compute_process_level_metrics(simulation_path, config)
-        
-        """ Build simulation summary """
-        summary = analysis_cproc.build_summary(config, metrics_info, msgs_dict, node_logs, [], min_tss, max_tss, avg_samples_per_node)
-        
-        """ Generate Figure """
-        plotting.plot_figure_cproc(msg_propagation_times, max_cpu_usage, max_memory_usage, total_network_usage, max_disk_usage, 
-                       msg_injection_times, summary['general'], summary['parameters'])
-        
-        """ Export summary """
-        analysis_cproc.export_summary(simulation_path, summary)
-
-    # """ Other infrastructure types """
+        cproc.run(simulation_config, simulation_path, msgs_dict, node_logs, msg_propagation_times, msg_injection_times, min_tss, max_tss)
+    elif infra_type == 'cadvisor':
+        cadvisor.run(simulation_config, metrics, topology_info, msg_propagation_times, msg_injection_times, min_tss, max_tss, prom_port)
     else:
-        analysis.inject_metric_in_dict(metrics, "propagation", "Propagation Time (per message)", "Propagation Time (ms)",
-                         "msg_propagation_times", msg_propagation_times)
-        analysis.inject_metric_in_dict(metrics, "injection", "Injection Time (per message)", "Milliseconds (ms)",
-                      "msg_injection_times", msg_injection_times)
-
-        prometheus.get_hardware_metrics(metrics, topology_info, min_tss, max_tss, prom_port)
-
-        """ Generate Figure """
-        plotting.plot_figure_ex(metrics, simulation_config)
+        analysis_logger.G_LOGGER.error(f'Unknown infrastructure type: {infra_type}')
 
     """ We are done """
     analysis_logger.G_LOGGER.info('Ended')
