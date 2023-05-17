8fafb3a84737efa120603f7f36210925f7fd502f
Alberto <alberto.soutullo639@gmail.com>
2023-05-17 17:01:22 +0200

    Modified tests

--- analysis-module/tests/test_prometheus.py
+++ analysis-module/tests/test_prometheus.py
@@ -6,6 +6,7 @@ from prometheus_api_client import PrometheusConnect
 
 # Project Imports
 from src import prometheus
+from src import plotting_configurations
 
 
 class TestPrometheus(unittest.TestCase):
@@ -13,24 +14,11 @@ class TestPrometheus(unittest.TestCase):
     @patch('src.prometheus.fetch_metric')
     def test_fetch_cadvisor_stats_from_prometheus_by_node(self, mock_fetch_metric):
         mock_fetch_metric.side_effect = [
-            [10, 20, 30],  # metric_1_name
-            [10, 20, 30],  # metric_2_name_1
-            [5, 15, 25]  # metric_2_name_2
+            [10, 20, 30]
         ]
 
         metrics = {
-            "by_node": {
-                "metric_1": {
-                    "metric_name": "metric_1_name",
-                    "statistic": "min",
-                    "toMB": False
-                },
-                "metric_2": {
-                    "metric_name": ["metric_2_name_1", "metric_2_name_2"],
-                    "statistic": "max",
-                    "toMB": False
-                }
-            }
+            "by_node": ["container_cpu_load_average_10s"]
         }
         prom = None
         container_ip = "192.168.1.1"
@@ -39,8 +27,7 @@ class TestPrometheus(unittest.TestCase):
 
         prometheus.fetch_cadvisor_stats_from_prometheus_by_node(metrics, prom, container_ip, start_ts, end_ts)
 
-        self.assertEqual(metrics["by_node"]["metric_1"]["values"], [10])
-        self.assertEqual(metrics["by_node"]["metric_2"]["values"], [[30], [25]])
+        self.assertEqual(plotting_configurations.plotting_config["container_cpu_load_average_10s"]["values"], [30])
 
     @patch.object(PrometheusConnect, 'custom_query_range')
     def test_fetch_metric(self, mock_custom_query_range):
@@ -92,28 +79,10 @@ class TestPrometheus(unittest.TestCase):
     @patch('src.prometheus.fetch_accumulated_metric_for_all_nodes')
     def test_fetch_cadvisor_stats_from_prometheus_by_simulation(self, mock_fetch_accumulated_metric_for_all_nodes):
         metrics = {
-            "by_simulation": {
-                "bandwith": {
-                    "metric_name": [
-                        "container_network_receive_bytes_total",
-                        "container_network_transmit_bytes_total"
-                    ],
-                    "toMB": True
-                }
-            }
-        }
-
-        expected_metrics = {
-            "by_simulation": {
-                "bandwith": {
-                    "metric_name": [
-                        "container_network_receive_bytes_total",
-                        "container_network_transmit_bytes_total"
-                    ],
-                    "values": [[12, 17, 13, 5, 8, 4], [1, 4, 3]],
-                    "toMB": True
-                }
-            }
+            "by_simulation": [
+                "container_network_receive_bytes_total",
+                "container_network_transmit_bytes_total"
+            ]
         }
 
         mock_fetch_accumulated_metric_for_all_nodes.side_effect = [[12, 17, 13, 5, 8, 4], [1, 4, 3]]
@@ -121,59 +90,10 @@ class TestPrometheus(unittest.TestCase):
 
         prometheus.fetch_cadvisor_stats_from_prometheus_by_simulation(metrics, prom, None, 0, 0)
 
-        self.assertEqual(metrics, expected_metrics)
-
-    @patch('src.prometheus.fetch_accumulated_metric_for_all_nodes')
-    def test_fetch_cadvisor_stats_from_prometheus_by_simulation_multiple(self,
-                                                                         mock_fetch_accumulated_metric_for_all_nodes):
-        metrics = {
-            "by_simulation": {
-                "bandwith": {
-                    "metric_name": [
-                        "container_network_receive_bytes_total",
-                        "container_network_transmit_bytes_total"
-                    ],
-                    "toMB": True
-                },
-                "disk": {
-                    "metric_name": [
-                        "container_fs_reads_bytes_total",
-                        "container_fs_writes_bytes_total"
-                    ],
-                    "toMB": True
-                }
-            }
-        }
-
-        expected_metrics = {
-            "by_simulation": {
-                "bandwith": {
-                    "metric_name": [
-                        "container_network_receive_bytes_total",
-                        "container_network_transmit_bytes_total"
-                    ],
-                    "values": [[5, 6, 5, 4, 8], [1, 4, 3]],
-                    "toMB": True
-                },
-                "disk": {
-                    "metric_name": [
-                        "container_fs_reads_bytes_total",
-                        "container_fs_writes_bytes_total"
-                    ],
-                    "values": [[7, 3, 1, 5, 10], [4, 7, 1]],
-                    "toMB": True
-                }
-            }
-        }
-
-        mock_fetch_accumulated_metric_for_all_nodes.side_effect = [[5, 6, 5, 4, 8], [1, 4, 3],
-                                                                   [7, 3, 1, 5, 10], [4, 7, 1],
-                                                                   ]
-        prom = PrometheusConnect()
-
-        prometheus.fetch_cadvisor_stats_from_prometheus_by_simulation(metrics, prom, None, 0, 0)
-
-        self.assertEqual(metrics, expected_metrics)
+        self.assertEqual(plotting_configurations.plotting_config["container_network_receive_bytes_total"]["values"],
+                         [[12, 17, 13, 5, 8, 4]])
+        self.assertEqual(plotting_configurations.plotting_config["container_network_transmit_bytes_total"]["values"],
+                         [[1, 4, 3]])
 
     @patch('src.prometheus.fetch_metric_with_timestamp')
     def test_fetch_accumulated_metric_for_all_nodes_1(self, mock_fetch_metric_with_timestamp):
@@ -234,7 +154,8 @@ class TestPrometheus(unittest.TestCase):
 
         metric_values = prometheus.fetch_metric_with_timestamp(prom, None, None, None, None)
         self.assertEqual(metric_values,
-                         [[1683281260, '5382144'], [1683281261, '5382144'], [1683281262, '5382144'], [1683281263, '6291456'],
+                         [[1683281260, '5382144'], [1683281261, '5382144'], [1683281262, '5382144'],
+                          [1683281263, '6291456'],
                           [1683281320, '10289152']])
 
     @patch.object(PrometheusConnect, 'custom_query_range')
