ef714041118f6d0a373e38dad5b2f1ed2b7907ff
Alberto <alberto.soutullo639@gmail.com>
2023-05-17 17:00:44 +0200

    Corrected plotting configurations

--- analysis-module/src/plotting_configurations.py
+++ analysis-module/src/plotting_configurations.py
@@ -11,42 +11,43 @@ plotting_config = {
         "statistic": "max",
         "toMB": True
     },
-    "cpu": {
-        "title": "Peak CPU Usage (per node)",
-        "y_label": "CPU Usage (%)",
-        "metric_name": "container_cpu_load_average_10s",
-        "statistic": "max",
-        "toMB": False
-    },
-    "memory": {
-        "title": "Peak Memory Usage (per node)",
-        "y_label": "Memory (MBytes)",
-        "metric_name": "container_memory_usage_bytes",
-        "statistic": "max",
+    "container_network_receive_bytes_total": {
+        "title": "Total Netowrk IO (per node)",
+        "y_label": "Bandwidth (MBytes)",
+        "xtic_labels": [
+            "Received (Rx)"
+        ],
         "toMB": True
     },
-    "bandwith": {
+    "container_network_transmit_bytes_total": {
         "title": "Total Netowrk IO (per node)",
         "y_label": "Bandwidth (MBytes)",
         "metric_name": [
-            "container_network_receive_bytes_total",
             "container_network_transmit_bytes_total"
         ],
         "xtic_labels": [
-            "Received (Rx)",
             "Sent (Tx)"
         ],
         "toMB": True
     },
-    "disk": {
+    "container_fs_reads_bytes_total": {
         "title": "Peak Disk IO (per node)",
         "y_label": "Disk IO (MBytes)",
         "metric_name": [
-            "container_fs_reads_bytes_total",
+            "container_fs_reads_bytes_total"
+        ],
+        "xtic_labels": [
+            "Read"
+        ],
+        "toMB": True
+    },
+    "container_fs_writes_bytes_total": {
+        "title": "Peak Disk IO (per node)",
+        "y_label": "Disk IO (MBytes)",
+        "metric_name": [
             "container_fs_writes_bytes_total"
         ],
         "xtic_labels": [
-            "Read",
             "Write"
         ],
         "toMB": True
