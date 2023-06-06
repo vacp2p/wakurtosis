b9669a23ba0289f4f83ad1d41917879bb8f283d0
0xFugue <119708655+0xFugue@users.noreply.github.com>
2023-06-06 12:20:23 +0530

    run.sh plot dirs

--- run.sh
+++ run.sh
@@ -204,15 +204,15 @@ docker cp "$wls_cid:/wls/messages.json" "./${enclave_name}_logs"
 if jq -e ."plotting" >/dev/null 2>&1 "./config/${wakurtosis_config_file}"; then
     if [ "$metrics_infra" = "dstats" ]; then
         docker run --name "dstats" --network "host" -v "$(pwd)/wakurtosis_logs:/simulation_data/" --add-host=host.docker.internal:host-gateway analysis src/hproc.py dstats /simulation_data/ --config-file /simulation_data/config/config.json  >/dev/null 2>&1  
-        docker cp dstats:/analysis/plots/ wakurtosis_logs/
+        docker cp dstats:/analysis/plots/ wakurtosis_logs/dstats-plots
         cd wakurtosis_logs
-        ln -s plots/output-dstats-compare.pdf analysis.pdf
+        ln -s dstats-plots/output-dstats-compare.pdf analysis.pdf
         cd ..
     elif [ "$metrics_infra" = "host-proc" ]; then
         docker run --name "host-proc" --network "host" -v "$(pwd)/wakurtosis_logs:/simulation_data/" --add-host=host.docker.internal:host-gateway analysis src/hproc.py host-proc /simulation_data/ --config-file /simulation_data/config/config.json  >/dev/null 2>&1
-        docker cp host-proc:/analysis/plots/ wakurtosis_logs/
+        docker cp host-proc:/analysis/plots/ wakurtosis_logs/host-proc-plots
         cd wakurtosis_logs
-        ln -s plots/output-host-proc-compare.pdf analysis.pdf
+        ln -s host-proc-plots/output-host-proc-compare.pdf analysis.pdf
         cd ..
     elif [ "$metrics_infra" = "container-proc" ]; then
         docker run --network "host" -v "$(pwd)/wakurtosis_logs:/simulation_data/" --add-host=host.docker.internal:host-gateway analysis src/main.py -i container-proc >/dev/null 2>&1
