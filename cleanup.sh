f626dd8aba5c81c13d57174fdf7f8c4af19d2ce3
Alberto Soutullo <alberto.soutullo639@gmail.com>
2023-06-09 12:22:10 +0200

    Cleaned bootstrap node and ENR in cleanup.sh

--- cleanup.sh
+++ cleanup.sh
@@ -5,10 +5,13 @@ rm -f   ./kurtosisrun_log.txt
 rm -f /tmp/hostproc-signal.fifo
 rm -rf  ./wakurtosis_logs ./config/topology_generated  ./monitoring/host-proc/stats
 
-docker stop gennet cadvisor > /dev/null  2>&1
-docker rm gennet cadvisor > /dev/null  2>&1
+docker stop gennet cadvisor bootstrap_node > /dev/null  2>&1
+docker rm gennet cadvisor bootstrap_node > /dev/null  2>&1
 
 kurtosis  --cli-log-level "error" enclave rm -f $enclave_name > /dev/null 2>&1
 
 docker stop $(docker ps -qa)  > /dev/null 2>&1
 docker rm $(docker ps -qa)  > /dev/null 2>&1
+
+toml_file="config/traits/discv5.toml"
+sed -i "s/^discv5-bootstrap-node=\".*\"$/discv5-bootstrap-node=""/" "$toml_file"
