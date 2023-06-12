dfe6588e437e12c896e93906ce1391a00459d772
Alberto Soutullo Rendo <alberto.soutullo639@gmail.com>
2023-06-12 18:00:50 +0200

    commented wrong lines

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 ## Install Docker
-#sudo apt-get update
-#sudo apt-get install docker-ce docker-ce-cli containerd.io docker-compose-plugin python3-venv
+sudo apt-get update
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-compose-plugin python3-venv
 
 apt-get install -y jq
 
@@ -19,32 +19,32 @@ else
   sudo rm /etc/apt/sources.list.d/kurtosis.list
 fi
 
-## Build the analysis docker image
-#cd analysis-module
-#sh ./build.sh
-#cd ..
-#
-## Build Gennet, WLS and Container-Proc monitoring docker images
-#
-#cd gennet-module
-#sh ./build_docker.sh
-#cd ..
-#
-#echo "host-proc: setup the venv @ /tmp/host-proc"
-#python3 -m venv /tmp/host-proc
-#. /tmp/host-proc/bin/activate
-#python3 -m pip install -r monitoring/host-proc/requirements.txt
-#deactivate
-#echo "host-proc: venv is ready"
-#
-#
-#cd wls-module
-#docker build -t wls:0.0.1 .
-#cd ..
-#
-#cd monitoring/container-proc
-#sh ./build.sh
-#cd ..
+# Build the analysis docker image
+cd analysis-module
+sh ./build.sh
+cd ..
+
+# Build Gennet, WLS and Container-Proc monitoring docker images
+
+cd gennet-module
+sh ./build_docker.sh
+cd ..
+
+echo "host-proc: setup the venv @ /tmp/host-proc"
+python3 -m venv /tmp/host-proc
+. /tmp/host-proc/bin/activate
+python3 -m pip install -r monitoring/host-proc/requirements.txt
+deactivate
+echo "host-proc: venv is ready"
+
+
+cd wls-module
+docker build -t wls:0.0.1 .
+cd ..
+
+cd monitoring/container-proc
+sh ./build.sh
+cd ..
 
 # enable as we start using go-waku
 
