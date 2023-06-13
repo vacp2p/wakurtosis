f0e391814f4b9a41909ae5a7b49b2a0cea04b382
0xFugue <119708655+0xFugue@users.noreply.github.com>
2023-06-13 17:24:22 +0530

    modify the fifo name

--- cleanup.sh
+++ cleanup.sh
@@ -2,8 +2,9 @@
 enclave_name=${1:-"wakurtosis"}
 # hardcoded files/fifo/folders
 rm -f   ./kurtosisrun_log.txt
-rm -f /tmp/hostproc-signal.fifo
+rm -f /tmp/host-proc-signal.fifo
 rm -rf  ./wakurtosis_logs ./config/topology_generated  ./monitoring/host-proc/stats ./monitoring/dstats/stats monitoring/container-proc/cproc_metrics.json
+
 
 docker stop gennet cadvisor dstats host-proc analysis > /dev/null  2>&1
 docker rm gennet cadvisor dstats host-proc analysis > /dev/null  2>&1
