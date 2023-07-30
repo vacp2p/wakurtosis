a000cf9a06a80f0c9c255c5f78071d11fc075373
Alberto Soutullo <alberto.soutullo639@gmail.com>
2023-07-30 14:25:00 +0200

    Deleted wrong located file

--- config/run_waku_node.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-IP=$(ip a | grep "inet " | grep -Fv 127.0.0.1 | sed 's/.*inet \([^/]*\).*/\1/')
-
-exec /usr/bin/wakunode --config-file="$1" --ports-shift="$2" --nat=extip:"${IP}" --log-level=TRACE
