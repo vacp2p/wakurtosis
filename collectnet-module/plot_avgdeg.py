1b21ed74de55b9ccccb5ead097583cb24741d551
0xFugue <119708655+0xFugue@users.noreply.github.com>
2023-08-17 10:27:55 +0530

    avgdeg

--- collectnet-module/plot_avgdeg.py
+++ collectnet-module/plot_avgdeg.py
@@ -7,14 +7,14 @@ import numpy as np
 import sys
 
 
-def process_network(json_fname):
+def process_network(json_fname, proto):
     avg_degrees = []
     with open(json_fname) as f:
         json_graphs, G = json.load(f), nx.empty_graph()
         for k in json_graphs.keys():
             js_graph = json_graphs[k]
             for src in js_graph.keys():
-                for dst  in js_graph[src]:
+                for dst  in js_graph[src][proto]:
                     G.add_edge(src, dst)
             avg_degrees.append(2 * len(G.edges)/len(G.nodes))
     return avg_degrees
@@ -45,9 +45,14 @@ def main(ctx: typer.Context,
          network_data_file: Path = typer.Option("observed_network.json",
              exists=True, file_okay=True, dir_okay=False, readable=True,
              help="Set network file"),
+         proto_name : str = typer.Option("relay",
+             help="Set the network fanout"),
+         proto_version : str = typer.Option("2.0.0",
+             help="Set the network fanout"),
          fanout : int = typer.Option(6,
              help="Set the network fanout")):
-    plot_network(process_network(network_data_file), fanout)
+    proto = f'/vac/waku/{proto_name}/{proto_version}'
+    plot_network(process_network(network_data_file, proto), fanout)
 
 
 if __name__ == "__main__":
